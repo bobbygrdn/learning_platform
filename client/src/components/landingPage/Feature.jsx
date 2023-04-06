e07baeb674544a8db86b64e135c65792741e7676
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:56:46 -0700

    finishes logic to display each feature icon and description

--- client/src/components/landingPage/Feature.jsx
+++ client/src/components/landingPage/Feature.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Feature() {
-    // TODO: Finish feature component
+export default function Feature({ feature }) {
     return (
         <>
-
+            <img className="logo" src={feature.Image} alt={`Feature ${feature.key}`} />
+            <p className="text">{feature.description}</p>
         </>
     )
 }
