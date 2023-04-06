7faaa08bde8cef413bc2a7e6db7e522436da40c1
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:44:37 -0700

    creates review test suite

---
+++ client/__tests__/Review.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Review from '../src/components/landingPage/Review';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/styles/Review.css', () => ({}));
+jest.mock('../src/styles/App.css', () => ({}));
+
+const review = () => {
+    return {
+        quote: 'This is a quote',
+        user: 'This is a user',
+    }
+}
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<Review review={review} />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+it('should render quote element with text class name', () => {
+    const wrapper = shallow(<Review review={review} />);
+    expect(wrapper.find('q.text')).toHaveLength(1);
+})
+
+it('should render paragraph element with user class name', () => {
+    const wrapper = shallow(<Review review={review} />);
+    expect(wrapper.find('p.user')).toHaveLength(1);
+})
