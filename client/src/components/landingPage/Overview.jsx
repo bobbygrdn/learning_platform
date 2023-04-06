6c2b3cf9950b577ce6464758bd016733928fb426
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 15:57:04 -0700

    finishes logic for overview

--- client/src/components/landingPage/Overview.jsx
+++ client/src/components/landingPage/Overview.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
 export default function Overview() {
-    // TODO: Finish overview component
     return (
-        <div>
-
-        </div>
+        <p className='text'>
+            Welcome to Valor Academy! This platform was created to engage and help guide the developer community into a new style of learning. Our focus is to allow developers in all stages of their journey to grow and become more confident in their abilities. As you progress through the different courses, lessons, quizzes and more, you will see that we focus on providing great content and a roadmap for you to level up!
+        </p>
     )
 }
