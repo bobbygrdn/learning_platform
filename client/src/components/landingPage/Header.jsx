d086c5d0f62e286572d5c50d9ba4a62b19490b5e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:34:46 -0700

    updates logic to use proper html header tag

--- client/src/components/landingPage/Header.jsx
+++ client/src/components/landingPage/Header.jsx
@@ -5,14 +5,14 @@ import '../../styles/App.css'
 export default function Header() {
     return (
         <>
-            <div className='header'>
+            <header className='header'>
                 <div className='titles'>
                     <p className="title">Valor Academy</p>
                     <p className="sectiontitle">Become a Code Warrior</p>
                 </div>
 
                 <img className="logo" src={process.env.PUBLIC_URL + '/resources/valorAcademy_logo.jpg'} alt="Valor Academy logo" width={"200px"} height={"200px"} />
-            </div >
+            </header >
         </>
     )
 }
