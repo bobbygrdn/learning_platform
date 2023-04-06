9c206a15e1cf8752a0090b4f09b70ed940da1847
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 14:16:12 -0700

    finishes component logic

--- client/src/components/landingPage/CallToAction.jsx
+++ client/src/components/landingPage/CallToAction.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../../styles/App.css'
 
 export default function CallToAction() {
-    // TODO: Finish call to action component
     return (
-        <div>
-
-        </div>
+        <>
+            <p className='subheading'>Choose your path to eternal glory and fame</p>
+            <p className='subheading'>Take on challenges, level up your knowledge and become a code warrior legend</p>
+            <p className='subheading'>Discover a better way to become triumphant in your coding journey</p>
+        </>
     )
 }
