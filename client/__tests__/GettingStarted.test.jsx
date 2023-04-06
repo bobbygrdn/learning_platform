f8703e509b9485150028277ff013279ad967501d
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:04:52 -0700

    creates getting started test suite

---
+++ client/__tests__/GettingStarted.test.jsx
@@ -0,0 +1,26 @@
+// Creates test suite for the getting started component using jest and enzyme
+
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import GettingStarted from '../src/components/landingPage/GettingStarted';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/styles/GettingStarted.css', () => ({}));
+jest.mock('../src/styles/App.css', () => ({}));
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<GettingStarted />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+it('should render four image elements with step class name', () => {
+    const wrapper = shallow(<GettingStarted />);
+    expect(wrapper.find('img.step')).toHaveLength(4);
+})
+
+it('should render three images of arrow icon with arrow class name', () => {
+    const wrapper = shallow(<GettingStarted />);
+    expect(wrapper.find('img.arrow')).toHaveLength(3);
+})
