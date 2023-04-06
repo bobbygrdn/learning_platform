b6d8e495699aa4f2a3f76dc93f5a46da8ccd3e3c
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 14:14:20 -0700

    builds callToAction test suite

---
+++ client/__tests__/CallToAction.test.jsx
@@ -0,0 +1,21 @@
+// creates test suite for the call to action component
+
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CallToAction from '../src/components/landingPage/CallToAction';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/styles/CallToAction.css', () => ({}));
+jest.mock('../src/styles/App.css', () => ({}));
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<CallToAction />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+it('should render paragraph elements', () => {
+    const wrapper = shallow(<CallToAction />);
+    expect(wrapper.find('p')).toHaveLength(3);
+})
