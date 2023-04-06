c2682b52e6d8e59c3b1b1ee92318b964193d1eaf
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 14:43:05 -0700

    creates button test suite

---
+++ client/__tests__/Button.test.jsx
@@ -0,0 +1,16 @@
+// creates test suite for button component
+
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CallToAction from '../src/components/landingPage/CallToAction';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/styles/CallToAction.css', () => ({}));
+jest.mock('../src/styles/App.css', () => ({}));
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<CallToAction />);
+    expect(wrapper).toMatchSnapshot();
+})
