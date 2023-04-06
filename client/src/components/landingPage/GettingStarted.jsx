d8ee369577a95b735b8f4088393e286828f38d99
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:05:15 -0700

    finishes logic to diplay step by step starter guide, still needs image urls

--- client/src/components/landingPage/GettingStarted.jsx
+++ client/src/components/landingPage/GettingStarted.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 export default function GettingStarted() {
-    // TODO: Finish GettingStarted component
+    // TODO: Add src urls for each arrow and each step
     return (
-        <div>
+        <>
+            <img className="step" src="" alt="Step 1" />
+            <img className="arrow" src="" alt="Arrow" />
+            <img className="step" src="" alt="Step 2" />
+            <img className="arrow" src="" alt="Arrow" />
+            <img className="step" src="" alt="Step 3" />
+            <img className="arrow" src="" alt="Arrow" />
+            <img className="step" src="" alt="Step 4" />
 
-        </div>
+        </>
     )
 }
