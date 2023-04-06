bc2e6cbf2ad47a09c0d06542186ef5ef23344df3
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:33:25 -0700

    finishes logic for footer to display all information and links

--- client/src/components/landingPage/Footer.jsx
+++ client/src/components/landingPage/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
 export default function Footer() {
-    // TODO: Finish footer component
     return (
-        <div>
+        <footer>
+            <p className="copyright">Copyright © 2023 Valor Academy. All rights reserved.</p>
+            <p className="media">Follow us on social media for news, updates, and promotions.</p>
+            <ul>
+                <li>
+                    <a href="/terms">Terms of Service</a>
+                    <p className='break'>|</p>
+                </li>
+                <li>
+                    <a href="/privacy">Privacy Policy</a>
+                    <p className='break'>|</p>
 
-        </div>
+                </li>
+                <li>
+                    <a href="https://github.com/bobbygrdn/robert_gordon_capstone.git">GitHub</a>
+                    <p className='break'>|</p>
+                </li>
+                <li>
+                    <a href="https://bobbygrdn.atlassian.net/wiki/spaces/VA/pages/7012354/Documentation?atlOrigin=eyJpIjoiODRkZmIzNWU2YjIxNGEyYjliZDAyMTVlYjExN2QyN2QiLCJwIjoiaiJ9">Jira</a>
+                </li>
+            </ul>
+        </footer>
     )
 }
