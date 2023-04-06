2c892d0f59d4bcce0c6e993afd8fe3113bc7b54e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 15:15:34 -0700

    creates user reviews test suite

---
+++ client/__tests__/UserReviews.test.jsx
@@ -0,0 +1,56 @@
+// creates test suite for userReviews component
+
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import UserReviews from '../src/components/landingPage/UserReviews';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/styles/Button.css', () => ({}));
+jest.mock('../src/styles/App.css', () => ({}));
+
+const reviews = () => {
+    return [
+        {
+            id: 1,
+            name: 'John Doe',
+            review: 'Great place',
+            rating: 5
+        },
+        {
+            id: 2,
+            name: 'Jane Doe',
+            review: 'Amazing place',
+            rating: 4
+        },
+        {
+            id: 3,
+            name: 'John Doe',
+            review: 'Amazing place',
+            rating: 5
+        },
+        {
+            id: 4,
+            name: 'John Doe',
+            review: 'Amazing place',
+            rating: 5
+        },
+        {
+            id: 5,
+            name: 'John Doe',
+            review: 'Amazing place',
+            rating: 5
+        }
+    ]
+}
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<UserReviews />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+it('renders the same number of reviews as the number of reviews passed in', () => {
+    const wrapper = shallow(<UserReviews reviews={reviews()} />);
+    expect(wrapper.find('Review')).toHaveLength(reviews().length);
+})
