32ef4ba34b7c2aaa2aad2821a2010c53a29286c1
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:13:14 -0700

    creates demo test suite

---
+++ client/__tests__/Demo.test.jsx
@@ -0,0 +1,22 @@
+// Creates test suite for the demo component using jest and enzyme
+
+import React from 'react';
+import ReactPlayer from 'react-player';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Demo from '../src/components/landingPage/Demo';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/styles/Demo.css', () => ({}));
+jest.mock('../src/styles/App.css', () => ({}));
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<Demo />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+it('should render a react player that plays the demo video', () => {
+    const wrapper = shallow(<Demo />);
+    expect(wrapper.find(ReactPlayer)).toHaveLength(1);
+})
