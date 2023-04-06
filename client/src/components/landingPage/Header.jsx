1bfb1e676d465fb580974c639ad726bdb23c9436
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 12:57:08 -0700

    finishes header component

--- client/src/components/landingPage/Header.jsx
+++ client/src/components/landingPage/Header.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Header() {
     // TODO: Finish header component
     return (
-        <div>
-
+        <div className='header'>
+            <h1 className="title">Valor Academy</h1>
+            <h2 className="subtitle">Become a Code Warrior</h2>
+            <img className="logo" src={process.env.PUBLIC_URL + '/resources/valorAcademy_logo.jpg'} alt="Valor Academy logo" width={"100px"} height={"100px"} />
         </div>
     )
 }
