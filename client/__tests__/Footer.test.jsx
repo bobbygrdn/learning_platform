57f6fee83acd8131752137ea26c9fe0a8dc47645
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-06 16:33:06 -0700

    creates footer test suite

---
+++ client/__tests__/Footer.test.jsx
@@ -0,0 +1,51 @@
+// Creates test suite for the demo component using jest and enzyme
+
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Footer from '../src/components/landingPage/Footer';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../src/styles/Footer.css', () => ({}));
+jest.mock('../src/styles/App.css', () => ({}));
+
+it('renders without crashing', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+it('should render paragraph tag with copyright classname', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('p.copyright')).toHaveLength(1);
+})
+
+it('should render footer element', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('footer')).toHaveLength(1);
+})
+
+it('should render paragraph tag with media classname', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('p.media')).toHaveLength(1);
+})
+
+it('should render unorganized list tag', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('ul')).toHaveLength(1);
+})
+
+it('should render 4 list item tags', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('li')).toHaveLength(4);
+})
+
+it('should render list tag with 4 anchor tags', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('a')).toHaveLength(4);
+})
+
+it('should render 3 paragraph tags with break class name', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('p.break')).toHaveLength(3);
+})
