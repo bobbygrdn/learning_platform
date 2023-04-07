8a6ce7246c7f9d20b3e756f4bff19c73160483f0
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-07 15:44:51 -0700

    adds css file

--- client/src/components/landingPage/Review.jsx
+++ client/src/components/landingPage/Review.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/landingPage/Review.css';
 
 export default function Review({ review }) {
-    // TODO: Finish user reviews component
     return (
         <>
-            <q className='text'>{review.quote}</q>
+            <q className='reviewText'>{review.quote}</q>
             <p className='user'>{review.user}</p>
         </>
     )
