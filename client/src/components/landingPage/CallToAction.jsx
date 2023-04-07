34c3d6e4b4d915bb809eff433a37d674b31b10b3
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-07 09:23:11 -0700

    Removes the app css file

--- client/src/components/landingPage/CallToAction.jsx
+++ client/src/components/landingPage/CallToAction.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../../styles/App.css'
 
 export default function CallToAction() {
     return (
--- client/src/components/landingPage/Header.jsx
+++ client/src/components/landingPage/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../../styles/Header.css'
-import '../../styles/App.css'
 
 export default function Header() {
     return (
