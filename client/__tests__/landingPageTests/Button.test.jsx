32c05cf124013706f1d9c2b7e54f50abf2ce9721
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-14 13:17:58 -0700

    moves test to landingPageTests directory

--- client/__tests__/landingPageTests/Button.test.jsx
+++ client/__tests__/landingPageTests/Button.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Button from '../src/components/landingPage/Button';
+import Button from '../../src/components/landingPage/Button';
 
 configure({ adapter: new Adapter() });
 
