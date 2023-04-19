04cecaa423bf1a87d71ff9e9acf69ba45b81b680
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-19 13:29:18 -0700

    Adds to do to update user reviews component

--- client/src/components/landingPage/UserReviews.jsx
+++ client/src/components/landingPage/UserReviews.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Review from './Review';
 import '../../styles/landingPage/UserReviews.css';
 export default function UserReviews({ reviews }) {
+    // TODO: Add loading state and render reviews after loading is complete
     return (
         <div className='reviews' data-testid="reviews">
             {reviews ? reviews.map((review, index) => {
