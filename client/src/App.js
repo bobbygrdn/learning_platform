3d7ff187b5e02485c0ddbc9571b02e694dd798b7
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-19 13:31:12 -0700

    adds react router, useEffect with localstorage call and passes token down to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import './styles/App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingPage/LandingPage';
+import Login from './components/authentication/Login';
+import Signup from './components/authentication/Signup';
+import Dashboard from './components/dashboard/dashboard';
+
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(localStorage.getItem('token'));
+  }, [token]);
+
   return (
     <div className='App'>
-      <LandingPage />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<LandingPage token={token} />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/dashboard' element={<Dashboard token={token} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
