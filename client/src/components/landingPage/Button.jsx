218599575922ad062357373d8257e959d5f2d7ed
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-24 11:27:08 -0700

    adds a disabled prop

--- client/src/components/landingPage/Button.jsx
+++ client/src/components/landingPage/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../../styles/landingPage/Button.css';
 
-export default function Button({ onClick, text, purpose, type }) {
+export default function Button({ onClick, text, purpose, type, disabled }) {
     return (
         <>
-            <button className={purpose} type={type} onClick={onClick} data-testid={purpose}>{text}</button>
+            <button className={purpose} type={type} onClick={onClick} data-testid={purpose} disabled={disabled}>{text}</button>
         </>
     )
 }
