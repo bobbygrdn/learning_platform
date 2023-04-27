e5340db9afe5e817105ee7f8032423751ebd656f
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-04-27 10:55:49 -0700

    removes old Spring Boot server

--- server/learning-platform/src/main/java/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import model.Course;
-
-public interface CourseRepository extends CrudRepository<Course, Integer> {
-
-}
