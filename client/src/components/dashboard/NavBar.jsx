a9696dbce624a47b97ffc603e3186be2071c8028
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-04 07:11:13 -0700

    adds logout button

--- client/src/components/dashboard/NavBar.jsx
+++ client/src/components/dashboard/NavBar.jsx
@@ -3,6 +3,11 @@ import { NavLink } from 'react-router-dom';
 import '../../styles/dashboard/NavBar.css';
 
 export default function NavBar() {
+
+    function logout() {
+        window.localStorage.clear()
+    }
+
     return (
         <ul className='navbar'>
             <NavLink className='catalog' to='/dashboard/catalog'>
@@ -20,6 +25,10 @@ export default function NavBar() {
             <NavLink className='profile' to='/dashboard/profile'>
                 <img className='navIcon' src={`${process.env.PUBLIC_URL + "/resources/profile_icon.png"}`} alt='Profile icon'></img>
                 <p className='labels'>Profile</p>
+            </NavLink>
+            <NavLink className='logout' to='/'>
+                <img className='navIcon' src={`${process.env.PUBLIC_URL + "/resources/logout_icon.png"}`} alt='Logout icon' onClick={logout}></img>
+                <p className='labels'>Logout</p>
             </NavLink>
         </ul>
     )
