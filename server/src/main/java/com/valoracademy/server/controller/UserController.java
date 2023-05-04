07b2846d2f8ec8540d164be676faef4a773b9a69
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-04 10:58:30 -0700

    adds a patch request mapping for user model

--- server/src/main/java/com/valoracademy/server/controller/UserController.java
+++ server/src/main/java/com/valoracademy/server/controller/UserController.java
@@ -49,6 +49,18 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
+    @PatchMapping("users/{id}/role")
+    public ResponseEntity<User> updateUserRole(@PathVariable("id") Long id,
+            @RequestBody User user) {
+        User existingUser = this.userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with the ID: " + id));
+
+        existingUser.setRole(user.getRole());
+
+        User updatedUser = this.userRepository.save(existingUser);
+        return ResponseEntity.ok(updatedUser);
+    }
+
     @DeleteMapping("users/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
         User user = this.userRepository.findById(id)
