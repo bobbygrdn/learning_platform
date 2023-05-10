df5eead5a1cead36c02a8ea6c68cbf945122b949
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-10 07:37:45 -0700

    Builds basic redirect page for admin dashboard login

---
+++ client/src/components/authentication/PlatformIssue.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PlatformIssue() {
+    return (
+        <div>
+            <h1>Sorry, you cannot use the admin dashboard on a mobile device.</h1>
+        </div>
+    )
+}
