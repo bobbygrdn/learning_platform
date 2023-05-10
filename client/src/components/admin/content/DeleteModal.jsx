4675a1d2050a918c9ab6cd5c00181c3d0422de19
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-10 14:04:34 -0700

    finishes the delete modal logic to handle delete requests

---
+++ client/src/components/admin/content/DeleteModal.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function DeleteModal({ deleteModalOpen, handleDeleteModal, currentEntity, table }) {
+    const deleteEntity = () => {
+
+        fetch(`/api/v1/${table.toLowerCase()}/${currentEntity.id}`, {
+            method: "DELETE"
+        })
+            .then(response => {
+                window.alert(`${currentEntity.title} has been deleted`);
+            })
+            .then(() => {
+                handleDeleteModal();
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    return (
+        <>
+            {deleteModalOpen ?
+                <div className='deleteModalWrapper'>
+                    <div className='deleteModal'>
+                        <button className='close' onClick={handleDeleteModal}>Close</button>
+                        <div className='modalContent'>
+                            <span>You are about to delete the following entity:</span>
+                            <span className='highlight'>{currentEntity.title}</span>
+                            <span>Are you sure you want to continue?</span>
+                        </div>
+                        <div className='buttons'>
+                            <button className='yes' onClick={deleteEntity}>Yes</button>
+                            <button className='no' onClick={handleDeleteModal}>No</button>
+                        </div>
+                    </div>
+                </div> : null}
+        </>
+    )
+}
