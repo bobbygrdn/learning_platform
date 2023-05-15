a42b67fdf028511417558d3aa64e7ad29b826bd2
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-15 15:04:34 -0700

    creates serviceImplementation for model

---
+++ server/src/main/java/com/robertgordon/valoracademy/service/impl/SettingsServiceImpl.java
@@ -0,0 +1,55 @@
+package com.robertgordon.valoracademy.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robertgordon.valoracademy.exception.ResourceNotFoundException;
+import com.robertgordon.valoracademy.model.Settings;
+import com.robertgordon.valoracademy.repository.SettingsRepository;
+import com.robertgordon.valoracademy.service.SettingsService;
+
+@Service
+public class SettingsServiceImpl implements SettingsService {
+
+    @Autowired
+    private SettingsRepository settingsRepository;
+
+    @Override
+    public Settings saveSettings(Settings settings) {
+        return settingsRepository.save(settings);
+    }
+
+    @Override
+    public List<Settings> getAllSettings() {
+        return settingsRepository.findAll();
+    }
+
+    @Override
+    public Settings getSettingsById(Long settingsId) {
+        Settings settings = settingsRepository.findById(settingsId)
+                .orElseThrow(() -> new ResourceNotFoundException("Settings does not exist with the ID: " + settingsId));
+        return settings;
+    }
+
+    @Override
+    public Settings updateSettings(Long id, Settings settings) {
+        Settings existingSettings = settingsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Settings does not exist with the ID: " + id));
+
+        existingSettings.setRegistrations(settings.isRegistrations());
+
+        Settings updatedSettings = settingsRepository.save(existingSettings);
+        return updatedSettings;
+    }
+
+    @Override
+    public void deleteSettings(Long id) {
+        Settings settings = settingsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Settings does not exist with the ID: " + id));
+
+        settingsRepository.delete(settings);
+    }
+
+}
