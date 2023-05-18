e7cac0f59b9082ac4387b5fc2a73747e2b31c22d
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-18 08:05:24 -0700

    updates controller to use model service

--- server/src/main/java/com/robertgordon/valoracademy/controller/Quiz_AnswerController.java
+++ server/src/main/java/com/robertgordon/valoracademy/controller/Quiz_AnswerController.java
@@ -6,66 +6,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.robertgordon.valoracademy.exception.ResourceNotFoundException;
 import com.robertgordon.valoracademy.model.Quiz_Answer;
-import com.robertgordon.valoracademy.repository.Quiz_AnswerRepository;
-import com.robertgordon.valoracademy.repository.Quiz_QuestionRepository;
+import com.robertgordon.valoracademy.service.Quiz_Answer_Service;
+import com.robertgordon.valoracademy.service.Quiz_QuestionService;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class Quiz_AnswerController {
 
-    // TODO: refactor this to implement the QuizAnswerService interface
     @Autowired
-    private Quiz_AnswerRepository quiz_answerRepository;
+    private Quiz_Answer_Service quiz_answerService;
 
     @Autowired
-    private Quiz_QuestionRepository quiz_questionRepository;
+    private Quiz_QuestionService quiz_questionService;
 
     @PostMapping("/questions/{questionid}/answers")
     public ResponseEntity<Quiz_Answer> createQuiz_Answer(@PathVariable("questionid") Long quizquestionid,
             @RequestBody Quiz_Answer quiz_answerRequest) {
-        Quiz_Answer quiz_answer = this.quiz_questionRepository.findById(quizquestionid).map(quizquestion -> {
-            quizquestion.getAnswers().add(quiz_answerRequest);
-            return quiz_answerRepository.save(quiz_answerRequest);
-        }).orElseThrow(() -> new ResourceNotFoundException("Quiz does not exist with the ID: " + quizquestionid));
+
+        this.quiz_questionService.getQuiz_QuestionById(quizquestionid).getAnswers().add(quiz_answerRequest);
+
+        Quiz_Answer quiz_answer = this.quiz_answerService.saveQuiz_Answer(quiz_answerRequest);
 
         return ResponseEntity.ok(quiz_answer);
     }
 
     @GetMapping("answers")
     public List<Quiz_Answer> getAllQuiz_Answers() {
-        return this.quiz_answerRepository.findAll();
+        return this.quiz_answerService.getAllQuiz_Answers();
     }
 
     @GetMapping("answers/{id}")
     public ResponseEntity<Quiz_Answer> getQuiz_AnswerById(@PathVariable("id") Long quiz_AnswerId) {
-        Quiz_Answer quiz_answer = this.quiz_answerRepository.findById(quiz_AnswerId)
-                .orElseThrow(() -> new ResourceNotFoundException(
-                        "Quiz_Answer does not exist with the ID: " + quiz_AnswerId));
+
+        Quiz_Answer quiz_answer = this.quiz_answerService.getQuiz_AnswerById(quiz_AnswerId);
         return ResponseEntity.ok(quiz_answer);
     }
 
     @PutMapping("/answers/{id}")
     public ResponseEntity<Quiz_Answer> updateQuiz_Answer(@PathVariable("id") Long quiz_AnswerId,
             @RequestBody Quiz_Answer quiz_answer) {
-        Quiz_Answer existingQuiz_Answer = this.quiz_answerRepository.findById(quiz_AnswerId)
-                .orElseThrow(() -> new ResourceNotFoundException(
-                        "Quiz_Answer does not exist with the ID: " + quiz_AnswerId));
 
-        existingQuiz_Answer.setContent(quiz_answer.getContent());
-        Quiz_Answer updatedQuiz_Answer = this.quiz_answerRepository.save(existingQuiz_Answer);
+        Quiz_Answer updatedQuiz_Answer = this.quiz_answerService.updateQuiz_Answer(quiz_AnswerId, quiz_answer);
 
         return ResponseEntity.ok(updatedQuiz_Answer);
     }
 
     @DeleteMapping("/answers/{id}")
     public ResponseEntity<String> deleteQuiz_Answer(@PathVariable("id") Long quiz_AnswerId) {
-        Quiz_Answer quiz_answer = this.quiz_answerRepository.findById(quiz_AnswerId)
-                .orElseThrow(() -> new ResourceNotFoundException(
-                        "Quiz_Answer does not exist with the ID: " + quiz_AnswerId));
 
-        this.quiz_answerRepository.delete(quiz_answer);
+        this.quiz_answerService.deleteQuiz_Answer(quiz_AnswerId);
         return ResponseEntity.ok("Quiz Answer with ID: " + quiz_AnswerId + " has been deleted");
     }
 
