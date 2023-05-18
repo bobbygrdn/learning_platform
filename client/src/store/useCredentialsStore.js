36f1f67908d3b9d300650affcdea3b71a8112ed6
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-18 09:39:20 -0700

    creates zustand store for user credentials state management

---
+++ client/src/store/useCredentialsStore.js
@@ -0,0 +1,16 @@
+import { createStore } from 'zustand';
+
+const useCredentialStore = createStore((set) => ({
+    userName: '',
+    password: '',
+    userId: '',
+    keepLoggedIn: false,
+    disabled: true,
+    setUserName: (name) => set({ userName: name }),
+    setPassword: (pass) => set({ password: pass }),
+    setUserId: (id) => set({ userId: id }),
+    setKeepLoggedIn: (keep) => set({ keepLoggedIn: keep }),
+    setDisabled: (disable) => set({ disabled: disable }),
+}));
+
+export default useCredentialStore;
