0688d90998690c699d216ab2895ca2400c2b50a1
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-22 14:48:19 -0700

    changes logic to match other content components

--- client/src/components/admin/content/EditContent.jsx
+++ client/src/components/admin/content/EditContent.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { useStore } from 'zustand';
 import useTableStore from '../../../store/useTableStore';
 
-export default function EditContent({ editModalOpen, handleEditModal }) {
+export default function EditContent({ handleModalOpen, table }) {
     const { currentEntity } = useStore(useTableStore)
 
     return (
         <>
-            {editModalOpen ?
-                <div className='editModalWrapper'>
-                    <p>Edit</p>
-                    <button onClick={handleEditModal}>Close</button>
-                </div> : null}
+            <div className='editContent'>
+                <button className='close' onClick={handleModalOpen}>Close</button>
+                <div className='modalContent'>
+                    <span>You are about to edit the following:</span>
+                    <span className='highlight'>{currentEntity.title}</span>
+                    <span>Are you sure you want to continue?</span>
+                </div>
+            </div>
         </>
 
     )
