921221082f976df56d65bd7805d06be53d270ff3
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-24 11:58:36 -0700

    Creates new AdminHeader for the admin dashboard

---
+++ client/src/components/admin/AdminHeader.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function AdminHeader() {
+
+    function logout() {
+        window.localStorage.clear()
+    }
+
+    const handleClick = () => {
+        const sidebarOpen = document.querySelector('#sidebarOpen');
+
+        sidebarOpen.classList.toggle('close');
+    }
+
+    return (
+        <nav className='adminNavbar'>
+            <div className="logo_item">
+                <i className="bx bx-menu" id="sidebarOpen" onClick={handleClick}></i>
+                Valor Academy
+                <img className='adminLogo' src={`${process.env.PUBLIC_URL + "/resources/valorAcademy_logo.png"}`} alt="" />
+            </div>
+
+            <NavLink className='logout_container' to='/'>
+                <p className='logout_text'>Logout</p>
+                <img className='logout' src={`${process.env.PUBLIC_URL + "/resources/logout_icon.png"}`} alt='Logout icon' onClick={logout}></img>
+            </NavLink>
+        </nav>
+    )
+}
