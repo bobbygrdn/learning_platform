6a35b426db3149fbc43cbf4e94ce2eb6a4a46a0a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-24 11:58:50 -0700

    Creates the admin sidebar for the admin dashboard

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -4,29 +4,70 @@ import '../../styles/admin/AdminNavBar.css';
 
 export default function AdminNavbar() {
 
-    function logout() {
-        window.localStorage.clear()
+    const handleMouseEnter = () => {
+        const sidebar = document.querySelector('.sidebar');
+
+        if (sidebar.classList.contains("hoverable")) {
+            sidebar.classList.remove("sidebarClose");
+        }
     }
 
-    return (
-        <ul className='navbar'>
-            <NavLink className='content' to='/admin/content'>
-                <img className='navIcon' src={`${process.env.PUBLIC_URL + "/resources/content_icon.png"}`} alt='Content icon'></img>
-                <p className='labels'>Content</p>
-            </NavLink>
-            <NavLink className='analytics' to='/admin/analytics'>
-                <img className='navIcon' src={`${process.env.PUBLIC_URL + "/resources/analytics_icon.png"}`} alt='Analytics icon'></img>
-                <p className='labels'>Analytics</p>
-            </NavLink>
-            <NavLink className='settings' to='/admin/settings'>
-                <img className='navIcon' src={`${process.env.PUBLIC_URL + "/resources/settings_icon.png"}`} alt='Settings icon'></img>
-                <p className='labels'>Settings</p>
-            </NavLink>
-            <NavLink className='logout' to='/'>
-                <img className='navIcon' src={`${process.env.PUBLIC_URL + "/resources/logout_icon.png"}`} alt='Logout icon' onClick={logout}></img>
-                <p className='labels'>Logout</p>
-            </NavLink>
-        </ul>
+    const handleMouseLeave = () => {
+        const sidebar = document.querySelector('.sidebar');
 
+        if (sidebar.classList.contains("hoverable")) {
+            sidebar.classList.add("sidebarClose");
+        }
+    }
+
+    const handleCollapse = () => {
+        const sidebar = document.querySelector('.sidebar');
+
+        sidebar.classList.add('sidebarClose', 'hoverable');
+    }
+
+    const handleExpand = () => {
+        const sidebar = document.querySelector('.sidebar');
+
+        sidebar.classList.remove('sidebarClose', 'hoverable');
+    }
+
+
+    return (
+        <aside className='sidebar sidebarClose hoverable' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className='menu_content'>
+                <ul className='menu_items'>
+                    <li className="item">
+                        <NavLink className='nav_link' to='/admin/content'>
+                            <img className='navlink_icon' src={`${process.env.PUBLIC_URL + "/resources/content_icon.png"}`} alt='Content icon'></img>
+                            <p className='navlink'>Content</p>
+                        </NavLink>
+                    </li>
+                    <li className="item">
+                        <NavLink className='nav_link' to='/admin/analytics'>
+                            <img className='navlink_icon' src={`${process.env.PUBLIC_URL + "/resources/analytics_icon.png"}`} alt='Analytics icon'></img>
+                            <p className='navlink'>Analytics</p>
+                        </NavLink>
+                    </li>
+                    <li className="item">
+                        <NavLink className='nav_link' to='/admin/settings'>
+                            <img className='navlink_icon' src={`${process.env.PUBLIC_URL + "/resources/settings_icon.png"}`} alt='Settings icon'></img>
+                            <p className='navlink'>Settings</p>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="bottom_content">
+                <div className="bottom expand_sidebar" onClick={handleExpand}>
+                    <span>Keep Open</span>
+                    <i className='bx bx-log-in'></i>
+                </div>
+                <div className="bottom collapse_sidebar" onClick={handleCollapse}>
+                    <span>Keep Closed</span>
+                    <i className='bx bx-log-out'></i>
+                </div>
+            </div>
+        </aside>
     )
 }
