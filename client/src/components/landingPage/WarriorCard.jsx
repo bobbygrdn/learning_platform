138469aa365933dee265bc9c08e173158e6c77c3
Robert Gordon <bobbygrdn777@gmail.com>
2023-05-27 22:30:27 -0700

    updates landing page styles and components to be more responsive and user friendly

--- client/src/components/landingPage/WarriorCard.jsx
+++ client/src/components/landingPage/WarriorCard.jsx
@@ -1,45 +1,54 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { Image } from "react-bootstrap";
 
 export default function WarriorCard({ warrior }) {
-    const [warrior1, setWarrior1] = React.useState('');
-    const [warrior2, setWarrior2] = React.useState('');
-    const [isHovered, setIsHovered] = React.useState(false);
+  const [warrior1, setWarrior1] = React.useState("");
+  const [warrior2, setWarrior2] = React.useState("");
+  const [isHovered, setIsHovered] = React.useState(false);
 
-    useEffect(() => {
-        switch (warrior) {
-            case "Samurai":
-                setWarrior1(process.env.PUBLIC_URL + '/resources/male_meijin.jpg');
-                setWarrior2(process.env.PUBLIC_URL + '/resources/female_meijin.jpg');
-                break;
-            case "Legionnaire":
-                setWarrior1(process.env.PUBLIC_URL + '/resources/female_imperator.jpg');
-                setWarrior2(process.env.PUBLIC_URL + '/resources/male_imperator.jpg');
-                break;
-            case "Spartan":
-                setWarrior1(process.env.PUBLIC_URL + '/resources/male_king.jpg');
-                setWarrior2(process.env.PUBLIC_URL + '/resources/female_king.jpg');
-                break;
-            default:
-                setWarrior1(process.env.PUBLIC_URL + '/resources/female_king_viking.jpg');
-                setWarrior2(process.env.PUBLIC_URL + '/resources/male_king_viking.jpg');
-                break;
-        }
-    }, [warrior]);
-
-    const handleMouseEnter = () => {
-        setIsHovered(true);
+  useEffect(() => {
+    switch (warrior) {
+      case "Samurai":
+        setWarrior1(process.env.PUBLIC_URL + "/resources/male_meijin.jpg");
+        setWarrior2(process.env.PUBLIC_URL + "/resources/female_meijin.jpg");
+        break;
+      case "Legionnaire":
+        setWarrior1(process.env.PUBLIC_URL + "/resources/female_imperator.jpg");
+        setWarrior2(process.env.PUBLIC_URL + "/resources/male_imperator.jpg");
+        break;
+      case "Spartan":
+        setWarrior1(process.env.PUBLIC_URL + "/resources/male_king.jpg");
+        setWarrior2(process.env.PUBLIC_URL + "/resources/female_king.jpg");
+        break;
+      default:
+        setWarrior1(
+          process.env.PUBLIC_URL + "/resources/female_king_viking.jpg"
+        );
+        setWarrior2(process.env.PUBLIC_URL + "/resources/male_king_viking.jpg");
+        break;
     }
+  }, [warrior]);
 
-    const handleMouseLeave = () => {
-        setIsHovered(false);
-    }
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
 
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
-
-    return (
-        <>
-            <img className="card" src={isHovered ? warrior1 : warrior2} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} data-testid="card" alt='warrior image' />
-        </>
-    )
+  return (
+    <div className="warriorCard">
+      <h1 className="warriorTitle">{warrior}</h1>
+      <Image
+        className="card"
+        src={isHovered ? warrior1 : warrior2}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        data-testid="card"
+        alt="warrior image"
+      />
+    </div>
+  );
 }
--- client/src/components/landingPage/WarriorCards.jsx
+++ client/src/components/landingPage/WarriorCards.jsx
@@ -1,22 +1,16 @@
-import React from 'react'
-import WarriorCard from './WarriorCard'
-import '../../styles/landingPage/WarriorCard.css'
+import React from "react";
+import WarriorCard from "./WarriorCard";
+import "../../styles/landingPage/WarriorCard.css";
 
 export default function WarriorCards() {
-    return (
-        <div className='warriorCards'>
-            <ul className='warriorList' data-testid="warriorList">
-                <li className="warriorName" data-testid="warriorName">Samurai</li>
-                <li className="warriorName" data-testid="warriorName">Legionnaire</li>
-                <li className="warriorName" data-testid="warriorName">Spartan</li>
-                <li className="warriorName" data-testid="warriorName">Viking</li>
-            </ul>
-            <div className='warriors'>
-                <WarriorCard warrior="Samurai" />
-                <WarriorCard warrior="Legionnaire" />
-                <WarriorCard warrior="Spartan" />
-                <WarriorCard warrior="Viking" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="warriorCards">
+      <div className="warriors">
+        <WarriorCard warrior="Samurai" />
+        <WarriorCard warrior="Legionnaire" />
+        <WarriorCard warrior="Spartan" />
+        <WarriorCard warrior="Viking" />
+      </div>
+    </div>
+  );
 }
