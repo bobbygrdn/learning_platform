12c477b55dde94740bb896888ea407b2a03af228
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-31 15:30:15 -0700

    adds seed file for dummy data injection

---
+++ server/src/main/resources/dummyData/seed.sql
@@ -0,0 +1,171 @@
+-- Insert statement for the course
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+-- INSERT INTO courses (title, description, user_id)
+-- VALUES ('Your First Course', 'This introductory course will walk you through using our platform.', 1);
+
+-- -- Insert statements for the lessons
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 12);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 13);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 14);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 15);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 16);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 17);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 18);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 19);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 20);
+-- INSERT INTO lessons (title, description, content, course_id)
+-- VALUES ('Your First Lesson', 'This introductory lesson will walk you through our User Interface for our platform and how to navigate through it.', 'These are the steps you will need to be successful in using our platform.', 21);
+
+-- -- Insert statements for the quizzes
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 7);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 8);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 9);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 10);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 11);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 12);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 13);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 14);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 15);
+-- INSERT INTO quizzes (title, description, lesson_id)
+-- VALUES ('Your First Quiz', 'This quiz will test your ability to recollect how to use our platform and navigate through it.', 16);
+
+-- -- Insert statements for the questions
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 7);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 8);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 9);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 10);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 11);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 12);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 13);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 14);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 15);
+-- INSERT INTO questions (title, content, quiz_id)
+-- VALUES ('Your First Question', 'What is the first screen you see when you login to the platform?', 16);
+
+-- -- Insert statements for the question options
+-- INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 10),
+--   ('A 500 Internal Server Error screen.', 10),
+--   ('Welcome Aboard screen.', 10),
+--   ('User Dashboard screen.', 10);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 11),
+--   ('A 500 Internal Server Error screen.', 11),
+--   ('Welcome Aboard screen.', 11),
+--   ('User Dashboard screen.', 11);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 12),
+--   ('A 500 Internal Server Error screen.', 12),
+--   ('Welcome Aboard screen.', 12),
+--   ('User Dashboard screen.', 12);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 13),
+--   ('A 500 Internal Server Error screen.', 13),
+--   ('Welcome Aboard screen.', 13),
+--   ('User Dashboard screen.', 13);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 14),
+--   ('A 500 Internal Server Error screen.', 14),
+--   ('Welcome Aboard screen.', 14),
+--   ('User Dashboard screen.', 14);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 15),
+--   ('A 500 Internal Server Error screen.', 15),
+--   ('Welcome Aboard screen.', 15),
+--   ('User Dashboard screen.', 15);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 16),
+--   ('A 500 Internal Server Error screen.', 16),
+--   ('Welcome Aboard screen.', 16),
+--   ('User Dashboard screen.', 16);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 17),
+--   ('A 500 Internal Server Error screen.', 17),
+--   ('Welcome Aboard screen.', 17),
+--   ('User Dashboard screen.', 17);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 18),
+--   ('A 500 Internal Server Error screen.', 18),
+--   ('Welcome Aboard screen.', 18),
+--   ('User Dashboard screen.', 18);
+--   INSERT INTO options (content, question_id)
+-- VALUES
+--   ('A 404 screen.', 19),
+--   ('A 500 Internal Server Error screen.', 19),
+--   ('Welcome Aboard screen.', 19),
+--   ('User Dashboard screen.', 19);
+
+-- -- Insert statements for the question answers
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 10);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 11);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 12);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 13);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 14);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 15);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 16);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 17);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 18);
+INSERT INTO answers (content, question_id)
+VALUES ('User Dashboard screen.', 19);
