ca5a4efa007c1b680f550437806b3e893db5eed4
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-31 15:32:12 -0700

    removes the delete content component

--- client/src/components/admin/content/Modal.jsx
+++ client/src/components/admin/content/Modal.jsx
@@ -3,7 +3,6 @@ import { useStore } from 'zustand';
 import useTableStore from '../../../store/useTableStore';
 import CreateContent from './CreateContent';
 import EditContent from './EditContent';
-import DeleteContent from './DeleteContent';
 
 export default function Modal({ table, handleModalOpen }) {
 
@@ -15,8 +14,6 @@ export default function Modal({ table, handleModalOpen }) {
                 return <CreateContent table={table} handleModalOpen={handleModalOpen} />
             case "edit":
                 return <EditContent handleModalOpen={handleModalOpen} />
-            case "delete":
-                return <DeleteContent handleModalOpen={handleModalOpen} />
             default:
                 return <h1>Error</h1>;
         }
