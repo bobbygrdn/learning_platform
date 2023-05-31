1d30193b624098adb6813cc2cece63c4d4768ecb
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-05-31 15:31:45 -0700

    changes component to be a card grid table

--- client/src/components/admin/content/Table.jsx
+++ client/src/components/admin/content/Table.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react';
 import { useStore } from 'zustand';
 import useTableStore from '../../../store/useTableStore';
 import Modal from './Modal';
+import { Card, Button } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 
 export default function Table({ searchTerm, table, setCurrentTable }) {
-    const { currentPage, setCurrentPage, setCurrentEntity, currentContent, setcurrentContent, courses, setCourses, lessons, setLessons, quizzes, setQuizzes, questions, setQuestions, modalOpen, setModalOpen, setAction } = useStore(useTableStore);
+    const { currentPage, setCurrentPage, currentEntity, setCurrentEntity, currentContent, setcurrentContent, courses, setCourses, lessons, setLessons, quizzes, setQuizzes, questions, setQuestions, modalOpen, setModalOpen, setAction } = useStore(useTableStore);
 
     useEffect(() => {
         fetch("/api/v1/courses")
@@ -59,105 +61,78 @@ export default function Table({ searchTerm, table, setCurrentTable }) {
         setModalOpen(!modalOpen);
     }
 
-    const selectedTable = () => {
-        switch (table) {
-            case "Lessons":
-                return <span>Quizzes</span>
-            case "Quizzes":
-                return <span>Questions</span>
-            case "Questions":
-                return <span>Information</span>
-            default:
-                return <span>Lessons</span>
+    const handleUserSelection = (action, id) => {
+        if (action === 'yes') {
+            fetch(`/api/v1/${table.toLowerCase()}/${id}`, {
+                method: "DELETE"
+            })
+                .then(response => {
+                    toast.success(`${currentContent.find((current) => current.id === id).title} has been deleted`);
+                    window.location.reload();
+                })
+                .catch(error => {
+                    toast.error(error.message);
+                })
+        } else {
+            toast.dismiss();
         }
     }
 
-    const selectedColumn = (selectTable) => {
-        switch (table) {
-            case "Lessons":
-                return <ul className='list'>{selectTable.quizzes.map((quiz, index) => <li key={index}>{index + 1}. {quiz.title}</li>)}</ul>
-            case "Quizzes":
-                return <ul className='list'>{selectTable.questions.map((question, index) => <li key={index}>{index + 1}. {question.title}</li>)}</ul>
-            case "Questions":
-                return <span>
-                    {selectTable.content}
-                    <p>Options:</p>
-                    <ul className='optionList'>
-                        {selectTable.options.map((option, index) => <li key={index}>{index + 1}. {option.content}</li>)}</ul>
-                    <p>{selectTable.answers.length === 1 ? "Answer:" : "Answers:"}</p>
-                    <ul className='answerList'>
-                        {selectTable.answers.map((answer, index) => <li key={index}>{answer.content}</li>)}</ul>
-                </span >
-            default:
-                return <ul className='list'>{selectTable.lessons.map((lesson, index) => <li key={index}>{index + 1}. {lesson.title}</li>)}</ul>
-        }
+    const handleDelete = (id) => {
+        toast.info(
+            <div>
+                <p>Are you sure you want to delete {currentContent.find((current) => current.id === id).title}?</p>
+                <div className='buttons'>
+                    <button className='yes' onClick={() => handleUserSelection('yes', id)}>Yes</button>
+                    <button className='no' onClick={() => handleUserSelection('no', id)}>No</button>
+                </div>
+            </div>,
+            {
+                autoClose: false,
+            }
+        )
     }
 
-    const handleRowClick = (event) => {
+    const handleCardClick = (event) => {
         // eslint-disable-next-line default-case
-        switch (table) {
-            case "Courses":
-                setCurrentTable("Lessons");
-                setcurrentContent(courses.find(course => course.id === parseInt(event.target.parentElement.id)).lessons);
-                setCurrentEntity(event.target.parentElement.id);
-                break;
-            case "Lessons":
-                setCurrentTable("Quizzes");
-                setcurrentContent(lessons.find(lesson => lesson.id === parseInt(event.target.parentElement.id)).quizzes);
-                setCurrentEntity(event.target.parentElement.id);
-                break;
-            case "Quizzes":
-                setCurrentTable("Questions");
-                setcurrentContent(quizzes.find(quiz => quiz.id === parseInt(event.target.parentElement.id), quizzes).questions)
-                setCurrentEntity(event.target.parentElement.id);
-                break;
-        }
+        // switch (table) {
+        //     case "Courses":
+        //         setCurrentTable("Lessons");
+        //         setcurrentContent(courses.find(course => course.id === parseInt(event.target.parentElement.id)).lessons);
+        //         setCurrentEntity(event.target.parentElement.id);
+        //         break;
+        //     case "Lessons":
+        //         setCurrentTable("Quizzes");
+        //         setcurrentContent(lessons.find(lesson => lesson.id === parseInt(event.target.parentElement.id)).quizzes);
+        //         setCurrentEntity(event.target.parentElement.id);
+        //         break;
+        //     case "Quizzes":
+        //         setCurrentTable("Questions");
+        //         setcurrentContent(quizzes.find(quiz => quiz.id === parseInt(event.target.parentElement.id), quizzes).questions)
+        //         setCurrentEntity(event.target.parentElement.id);
+        //         break;
+        // }
     }
 
     return (
-        <div className={`${table}Table`}>
+        <div className={`${table}Grid`}>
             <Modal table={table} handleModalOpen={handleModalOpen} />
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            <span>Title</span>
-                        </th>
-                        <th>
-                            {selectedTable()}
-                        </th>
-                        <th>
-                            <span>Action</span>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredContent.slice(startIndex, endIndex).map(thisTable => (
-                        <tr key={thisTable.id} id={thisTable.id}>
-                            <td onClick={handleRowClick}>
-                                <span>{thisTable.title}</span>
-                            </td>
-                            <td onClick={handleRowClick}>
-                                {selectedColumn(thisTable)}
-                            </td>
-                            <td>
-                                <button className='edit' type='button' onClick={() => handleModalOpen(thisTable.id, "edit")}>Edit</button>
-                                <button className='delete' type='button' onClick={() => handleModalOpen(thisTable.id, "delete")}>Delete</button>
-                            </td>
-                        </tr>
-                    ))}
-                    <tr>
-                        <td className='createButtonColumn' colSpan={3}>
-                            <button className='create' type='button' onClick={() => handleModalOpen(null, "create")}>Create</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <div>
-                {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
-                    <button className='pagesButton' key={page} onClick={() => setCurrentPage(page)}>{page}</button>
+            <div className="grid-container">
+                {filteredContent.slice(startIndex, endIndex).map(thisTable => (
+                    <Card className='Card' key={thisTable.id} id={thisTable.id} onClick={() => handleCardClick(thisTable.id)}>
+                        <Card.Body>
+                            <Card.Title>{thisTable.title}</Card.Title>
+                            <Card.Text>{ }</Card.Text>
+                        </Card.Body>
+                        <div className='buttons'>
+                            <Button className='edit' onClick={() => handleModalOpen(thisTable.id, "edit")}>Edit</Button>
+                            <Button className='delete' onClick={() => handleDelete(thisTable.id)}>Delete</Button>
+                        </div>
+                    </Card>
                 ))}
             </div>
+            <Button className='create' onClick={() => handleModalOpen(null, "create")}>Create</Button>
         </div>
+
     )
 }
