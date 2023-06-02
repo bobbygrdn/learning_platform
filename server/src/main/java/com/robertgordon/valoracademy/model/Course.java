d84d3044bf386ea1de5b07644b65439997961fb2
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-02 11:10:53 -0700

    adds new fields, constctor and getters/setters for new fields

--- server/src/main/java/com/robertgordon/valoracademy/model/Course.java
+++ server/src/main/java/com/robertgordon/valoracademy/model/Course.java
@@ -21,6 +21,15 @@ public class Course {
     @Column(name = "description", nullable = false)
     private String description;
 
+    @Column(name = "topics", nullable = false)
+    private String topics;
+
+    @Column(name = "difficulty", nullable = false)
+    private String difficulty;
+
+    @Column(name = "time_needed", nullable = false)
+    private String timeToComplete;
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "course_id")
     private List<Lesson> lessons = new ArrayList<>();
@@ -34,10 +43,13 @@ public class Course {
 
     }
 
-    public Course(long id, String title, String description) {
+    public Course(long id, String title, String description, String topics, String difficulty, String timeToComplete) {
         this.id = id;
         this.title = title;
         this.description = description;
+        this.topics = topics;
+        this.difficulty = difficulty;
+        this.timeToComplete = timeToComplete;
     }
 
     public long getId() {
@@ -80,6 +92,30 @@ public class Course {
         this.user = user;
     }
 
+    public String getTopics() {
+        return topics;
+    }
+
+    public void setTopics(String topics) {
+        this.topics = topics;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getTimeToComplete() {
+        return timeToComplete;
+    }
+
+    public void setTimeToComplete(String timeToComplete) {
+        this.timeToComplete = timeToComplete;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -87,7 +123,11 @@ public class Course {
         result = prime * result + (int) (id ^ (id >>> 32));
         result = prime * result + ((title == null) ? 0 : title.hashCode());
         result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((topics == null) ? 0 : topics.hashCode());
+        result = prime * result + ((difficulty == null) ? 0 : difficulty.hashCode());
+        result = prime * result + ((timeToComplete == null) ? 0 : timeToComplete.hashCode());
         result = prime * result + ((lessons == null) ? 0 : lessons.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
         return result;
     }
 
@@ -112,17 +152,39 @@ public class Course {
                 return false;
         } else if (!description.equals(other.description))
             return false;
+        if (topics == null) {
+            if (other.topics != null)
+                return false;
+        } else if (!topics.equals(other.topics))
+            return false;
+        if (difficulty == null) {
+            if (other.difficulty != null)
+                return false;
+        } else if (!difficulty.equals(other.difficulty))
+            return false;
+        if (timeToComplete == null) {
+            if (other.timeToComplete != null)
+                return false;
+        } else if (!timeToComplete.equals(other.timeToComplete))
+            return false;
         if (lessons == null) {
             if (other.lessons != null)
                 return false;
         } else if (!lessons.equals(other.lessons))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
             return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Course [id=" + id + ", title=" + title + ", description=" + description + ", lessons=" + lessons + "]";
+        return "Course [id=" + id + ", title=" + title + ", description=" + description + ", topics=" + topics
+                + ", difficulty=" + difficulty + ", timeToComplete=" + timeToComplete + ", lessons=" + lessons
+                + ", user=" + user + "]";
     }
 
 }
