6bca30cdc71fe0df2f3f4332a5093688d9b633cf
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-02 11:08:40 -0700

    adds new fields to be used in my content components

--- client/src/store/useModalStore.js
+++ client/src/store/useModalStore.js
@@ -3,9 +3,15 @@ import { createStore } from 'zustand';
 const useModalStore = createStore((set) => ({
     title: '',
     description: '',
+    difficulty: '',
+    timeNeeded: '',
+    topics: '',
     content: '',
     setTitle: (title) => set({ title }),
     setDescription: (description) => set({ description }),
+    setDifficulty: (difficulty) => set({ difficulty }),
+    setTimeNeeded: (timeNeeded) => set({ timeNeeded }),
+    setTopics: (topics) => set({ topics }),
     setContent: (content) => set({ content }),
 }));
 
