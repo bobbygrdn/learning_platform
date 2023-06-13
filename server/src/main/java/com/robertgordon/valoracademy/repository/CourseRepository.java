7e9a738c9f61abf982a8eabfe65197788655f1ef
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-13 12:16:45 -0700

    Adds two custom queries

--- server/src/main/java/com/robertgordon/valoracademy/repository/CourseRepository.java
+++ server/src/main/java/com/robertgordon/valoracademy/repository/CourseRepository.java
@@ -3,7 +3,12 @@ package com.robertgordon.valoracademy.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.robertgordon.valoracademy.model.Course;
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    public List<Course> findByDifficulty(String difficulty);
+
+    public List<Course> findByTimeToComplete(String timeToComplete);
 
 }
