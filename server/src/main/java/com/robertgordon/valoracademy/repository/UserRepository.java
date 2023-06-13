658d48470ede170648413600e6a7ae844481f6e2
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-13 12:16:53 -0700

    adds one custom query

--- server/src/main/java/com/robertgordon/valoracademy/repository/UserRepository.java
+++ server/src/main/java/com/robertgordon/valoracademy/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.robertgordon.valoracademy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.robertgordon.valoracademy.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public List<User> findByRole(String role);
 
 }
