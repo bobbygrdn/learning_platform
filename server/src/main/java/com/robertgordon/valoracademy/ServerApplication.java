91b819ef66d8f7b58d3bad5353ee4a3f39da814e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-13 09:51:57 -0700

    adds commandline runner bean

--- server/src/main/java/com/robertgordon/valoracademy/ServerApplication.java
+++ server/src/main/java/com/robertgordon/valoracademy/ServerApplication.java
@@ -1,13 +1,20 @@
 package com.robertgordon.valoracademy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> System.out.println("My applicaiton got started!!");
 	}
 
 }
