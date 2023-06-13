dc5964f56dcf88c7c94cd321216423a34be2b4d8
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-13 15:22:59 -0700

    Creates a password encoder class to encrypt passwords using BCrypt

---
+++ server/src/main/java/com/robertgordon/valoracademy/util/PasswordEncoder.java
@@ -0,0 +1,15 @@
+package com.robertgordon.valoracademy.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public static String encodePassword(String password) {
+        return encoder.encode(password);
+    }
+
+    public static boolean checkPassword(String rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword, encodedPassword);
+    }
+}
