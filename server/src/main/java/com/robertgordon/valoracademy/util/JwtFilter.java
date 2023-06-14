67333f1a9aed725cc6961675907a1e74024faff9
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-14 12:37:54 -0700

    Creates a JwtFilter class to filter and parse requests and responses from the servlet based on the provided token

---
+++ server/src/main/java/com/robertgordon/valoracademy/util/JwtFilter.java
@@ -0,0 +1,40 @@
+package com.robertgordon.valoracademy.util;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+public class JwtFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+        final String authHeader = request.getHeader("authorization");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(request, response);
+        } else {
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                throw new ServletException("An exception occurred");
+            }
+        }
+        final String token = authHeader.substring(7);
+        Claims claims = Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody();
+        request.setAttribute("claims", claims);
+        request.setAttribute("user", servletRequest.getParameter("id"));
+        filterChain.doFilter(request, response);
+    }
+
+}
