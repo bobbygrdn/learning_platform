55380c8d9a3ffbc8200b76d7c2f83babbe8d9418
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-14 12:36:31 -0700

    Adds JWT logic to the login controller to return a JSON Web Token to the client upon successful login

--- server/src/main/java/com/robertgordon/valoracademy/controller/LoginController.java
+++ server/src/main/java/com/robertgordon/valoracademy/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.robertgordon.valoracademy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,12 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.robertgordon.valoracademy.model.User;
 import com.robertgordon.valoracademy.repository.UserRepository;
+import com.robertgordon.valoracademy.util.JwtUtil;
 import com.robertgordon.valoracademy.util.PasswordEncoder;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class LoginController {
@@ -21,21 +18,20 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/api/v1/login")
-    public ResponseEntity<User> login(@RequestBody User user, HttpServletRequest request,
-            HttpServletResponse response) {
+    public ResponseEntity<?> login(@RequestBody User user) {
 
         User existingUser = userRepository.findByUsername(user.getUsername());
 
         if (existingUser != null && PasswordEncoder.checkPassword(user.getPassword(), existingUser.getPassword())) {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", existingUser);
-            Cookie cookie = new Cookie("userId", String.valueOf(existingUser.getId()));
-            if (user.isKeepLoggedIn() == true) {
-                cookie.setMaxAge(60 * 60 * 24 * 365 * 10);
-            }
-            response.addCookie(cookie);
+
+            return ResponseEntity.ok(jwtUtil.generateToken(existingUser));
+
+        } else {
+            return new ResponseEntity<>("Could not login user.", HttpStatus.CONFLICT);
         }
-        return ResponseEntity.ok(existingUser);
     }
 }
