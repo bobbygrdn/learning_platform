5458964c58472ef3d5924011b4a671050c0c6ac0
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-14 15:06:18 -0700

    Updates component to show the proper error and an explanation to the user

--- client/src/components/authentication/PlatformIssue.jsx
+++ client/src/components/authentication/PlatformIssue.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import '../../styles/authentication/Errors.css';
 
 export default function PlatformIssue() {
+
+    useEffect(() => {
+        localStorage.clear();
+    }, []);
+
     return (
-        <div>
-            <h1>Sorry, you cannot use the admin dashboard on a mobile device.</h1>
+        <div className='platformIssueWrapper'>
+            <img src={process.env.PUBLIC_URL + '/resources/valorAcademy_logo.png'} alt="Valor Academy logo" />
+            <h1>403 Access Denied</h1>
+            <p>You are currently using a mobile device.</p>
+            <p>Our policies prevent the use of admin personnel from using our admin dashboard on mobile devices.</p>
+            <p>Please login on a non-mobile device.</p>
         </div>
     )
 }
