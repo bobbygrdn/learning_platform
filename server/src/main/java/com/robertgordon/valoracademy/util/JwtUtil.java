77ad8c13907632a04900e4c15e6aec1cdb8c7bdf
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-15 12:29:40 -0700

    Adds comments to explain the class

--- server/src/main/java/com/robertgordon/valoracademy/util/JwtUtil.java
+++ server/src/main/java/com/robertgordon/valoracademy/util/JwtUtil.java
@@ -12,12 +12,23 @@ import com.robertgordon.valoracademy.model.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+/** `@Component` is used to mark the `JwtUtil` class as a Spring component so that it can be injected into other 
+* Spring-managed classes using dependency injection. */
 @Component
 public class JwtUtil {
 
+   /** `@Value("${jwt.secret}")` is a Spring annotation used to inject the value of the `jwt.secret`
+    * property from the application properties file into the `secret` variable. This allows the
+    * `secret` value to be easily changed without modifying the code. */
     @Value("${jwt.secret}")
     private String secret;
 
+    /**
+     * This method generates a JWT token for a given user and returns a map containing the token, user
+     * ID, and user role.
+     * @param existingUser an object of the User class that represents an existing user in the system.
+     * @return A Map object containing a JWT token, user ID, and user role.
+     */
     public Map<String, String> generateToken(User existingUser) {
 
         String jwtToken = "";
--- server/src/main/java/com/robertgordon/valoracademy/util/PasswordEncoder.java
+++ server/src/main/java/com/robertgordon/valoracademy/util/PasswordEncoder.java
@@ -3,12 +3,29 @@ package com.robertgordon.valoracademy.util;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoder {
+
+    /* This code creates a private static final instance of the `BCryptPasswordEncoder` class, which is used to
+     encode and decode passwords using the bcrypt hashing algorithm. */
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+    /**
+     * This method encodes a given password using a password encoder.
+     * @param password The password that needs to be encoded.
+     * @return The method `encodePassword` returns a String that is the encoded version of the input
+     * password.
+     */
     public static String encodePassword(String password) {
         return encoder.encode(password);
     }
 
+    /**
+     * This method checks if a raw password matches an encoded password using an encoder.
+     * @param rawPassword The raw (unencoded) password that is being checked against the encoded password.
+     * @param encodedPassword The encoded password is a string that has been previously encrypted or
+     * hashed using a specific algorithm.
+     * @return The method `checkPassword` returns a boolean value indicating whether the `rawPassword`
+     * matches the `encodedPassword` after being encoded using the `encoder` object.
+     */
     public static boolean checkPassword(String rawPassword, String encodedPassword) {
         return encoder.matches(rawPassword, encodedPassword);
     }
