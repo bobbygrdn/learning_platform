04f26b1df1e171db458962645eb4d6e245002cb1
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-16 12:21:04 -0700

    Changes logic to clear all data in localStorage and in the zustand store when logging out

--- client/src/components/admin/AdminHeader.jsx
+++ client/src/components/admin/AdminHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useStore } from 'zustand';
+import useAuthStore from '../../store/useAuthStore';
 
 export default function AdminHeader() {
 
+    const { setToken, setRole } = useStore(useAuthStore);
 
     function logout() {
         localStorage.clear();
+        setToken(null);
+        setRole(null);
     }
 
     return (
@@ -15,7 +20,7 @@ export default function AdminHeader() {
                 <img className='adminLogo' src={`${process.env.PUBLIC_URL + "/resources/valorAcademy_logo.png"}`} alt="" />
             </div>
 
-            <NavLink className='logout_container' to={'/'} onClick={logout}>
+            <NavLink className='logout_container' to={'/login'} onClick={logout}>
                 <span className='logout_text'>Logout</span>
                 <img className='logout' src={`${process.env.PUBLIC_URL + "/resources/logout_icon.png"}`} alt='Logout icon'></img>
             </NavLink>
