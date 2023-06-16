a6b6aa4f910cb1e73ee75ceed97b5dd0c69fca02
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-16 12:21:52 -0700

    imports the urlPaths and uses the constant variables for the addUrlPatterns method instead of hard coding the strings

--- server/src/main/java/com/robertgordon/valoracademy/FilterConfig.java
+++ server/src/main/java/com/robertgordon/valoracademy/FilterConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.robertgordon.valoracademy.util.UrlPaths;
 import com.robertgordon.valoracademy.util.JwtFilter;
 
 /** `@Configuration` is used to define the `FilterConfig` class as a configuration
@@ -22,7 +23,7 @@ public class FilterConfig {
         filter.setFilter(new JwtFilter());
 
         // provide endpoints which need to be restricted
-        filter.addUrlPatterns("/api/v1/users", "/api/v1/courses", "/api/v1/lessons", "/api/v1/quizzes", "/api/v1/questions", "/api/v1/options", "/api/v1/answers");
+        filter.addUrlPatterns(UrlPaths.USERS, UrlPaths.COURSES, UrlPaths.LESSONS, UrlPaths.QUIZZES, UrlPaths.QUESTIONS, UrlPaths.OPTIONS, UrlPaths.ANSWERS);
         return filter;
     }
 }
