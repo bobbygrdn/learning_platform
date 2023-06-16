9f68a0ec02d3b07a85490ac2ef678fb58ef4a708
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-16 12:20:36 -0700

    creates a class to hold the constant variables for the urls

---
+++ server/src/main/java/com/robertgordon/valoracademy/util/UrlPaths.java
@@ -0,0 +1,12 @@
+package com.robertgordon.valoracademy.util;
+
+public final class UrlPaths {
+    public static final String USERS = "/api/v1/users";
+    public static final String COURSES = "/api/v1/courses";
+    public static final String LESSONS = "/api/v1/lessons";
+    public static final String QUIZZES = "/api/v1/quizzes";
+    public static final String QUESTIONS = "/api/v1/questions";
+    public static final String OPTIONS = "/api/v1/options";
+    public static final String ANSWERS = "/api/v1/answers";
+    public static final String CLASSURL = "/api/v1/";
+}
