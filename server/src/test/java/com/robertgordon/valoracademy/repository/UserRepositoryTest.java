1a8cfdd97604a42c607d2e25f2ee733513512f36
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-20 08:16:28 -0700

    Removes unused import

--- server/src/test/java/com/robertgordon/valoracademy/repository/UserRepositoryTest.java
+++ server/src/test/java/com/robertgordon/valoracademy/repository/UserRepositoryTest.java
@@ -2,8 +2,6 @@ package com.robertgordon.valoracademy.repository;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
