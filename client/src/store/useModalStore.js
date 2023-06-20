9490e180bed51593af6f1d57a10f31eae064c155
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-20 14:09:44 -0700

    adds options and answers state with their update methods

--- client/src/store/useModalStore.js
+++ client/src/store/useModalStore.js
@@ -7,12 +7,16 @@ const useModalStore = createStore((set) => ({
     timeNeeded: '',
     topics: '',
     content: '',
+    options: '',
+    answers: '',
     setTitle: (title) => set({ title }),
     setDescription: (description) => set({ description }),
     setDifficulty: (difficulty) => set({ difficulty }),
     setTimeNeeded: (timeNeeded) => set({ timeNeeded }),
     setTopics: (topics) => set({ topics }),
     setContent: (content) => set({ content }),
+    setOptions: (options) => set({ options }),
+    setAnswers: (answers) => set({ answers }),
     addContent: (newContent) => set((state) => ({ ...state, content: state.content + newContent })),
 }));
 
