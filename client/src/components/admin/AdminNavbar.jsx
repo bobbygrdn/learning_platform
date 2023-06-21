2ef355cdf122f5b3facaca1fd094cb807b538329
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-21 14:25:04 -0700

    added logic to reset the view for the dashboard to the default view when a link is clicked

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useStore } from 'zustand';
+import useTableStore from '../../store/useTableStore';
 
 export default function AdminNavbar() {
+
+    const { setCurrentTable } = useStore(useTableStore);
 
     const handleMouseEnter = () => {
         const sidebar = document.querySelector('.sidebar');
@@ -19,24 +23,28 @@ export default function AdminNavbar() {
         }
     }
 
+    const handleClick = () => {
+        setCurrentTable("Courses");
+    }
+
     return (
         <aside className='sidebar sidebarClose hoverable' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className='menu_content'>
                 <ul className='menu_items'>
                     <li className="item">
-                        <NavLink className='nav_link' to='/admin/content'>
+                        <NavLink className='nav_link' to='/admin/creator' onClick={handleClick}>
                             <img className='navlink_icon' src={`${process.env.PUBLIC_URL + "/resources/content_icon.png"}`} alt='Content icon'></img>
-                            <p className='navlink'>Content</p>
+                            <p className='navlink'>Creator</p>
                         </NavLink>
                     </li>
                     <li className="item">
-                        <NavLink className='nav_link' to='/admin/analytics'>
+                        <NavLink className='nav_link' to='/admin/publisher' onClick={handleClick}>
                             <img className='navlink_icon' src={`${process.env.PUBLIC_URL + "/resources/analytics_icon.png"}`} alt='Analytics icon'></img>
-                            <p className='navlink'>Analytics</p>
+                            <p className='navlink'>Publisher</p>
                         </NavLink>
                     </li>
                     <li className="item">
-                        <NavLink className='nav_link' to='/admin/settings'>
+                        <NavLink className='nav_link' to='/admin/settings' onClick={handleClick}>
                             <img className='navlink_icon' src={`${process.env.PUBLIC_URL + "/resources/settings_icon.png"}`} alt='Settings icon'></img>
                             <p className='navlink'>Settings</p>
                         </NavLink>
