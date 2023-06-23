5b5369608b81b4e6aa0068df9a4e4b4d6b872e4e
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-23 15:32:52 -0700

    Adds default values for role and token that are pulled from localStorage

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,12 +1,18 @@
 import { createStore } from 'zustand';
 
-const useAuthStore = createStore((set) => ({
-    role: null,
-    setRole: (role) => set(() => ({ role })),
-    token: null,
-    setToken: (token) => set(() => ({ token })),
-    allowRegistration: false,
-    setAllowRegistration: (allowRegistration) => set(() => ({ allowRegistration: allowRegistration })),
-}));
+const useAuthStore = createStore((set) => {
+
+    const token = localStorage.getItem('token');
+    const role = localStorage.getItem('role');
+
+    return {
+        role: role || null,
+        setRole: (role) => set(() => ({ role })),
+        token: token || null,
+        setToken: (token) => set(() => ({ token })),
+        allowRegistration: false,
+        setAllowRegistration: (allowRegistration) => set(() => ({ allowRegistration: allowRegistration })),
+    }
+});
 
 export default useAuthStore;
