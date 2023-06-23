d0bcfc3f43e42d43bb2aa822faf87721e1d1724c
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-23 15:38:09 -0700

    Moves fetch requests from child component here to a higher level to simplify child components

--- client/src/components/admin/AdminDashboard.jsx
+++ client/src/components/admin/AdminDashboard.jsx
@@ -5,12 +5,66 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import '../../styles/admin/AdminDashboard.css';
 import { useStore } from 'zustand';
 import useAuthStore from '../../store/useAuthStore';
+import useTableStore from '../../store/useTableStore';
 
 export default function AdminDashboard() {
 
-    const { role } = useStore(useAuthStore);
+    const { role, token } = useStore(useAuthStore);
+    const { courses, setCourses, setLessons, setQuizzes, setQuestions, currentTable, setCurrentContent } = useStore(useTableStore);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch("/api/v1/courses", {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setCourses(data);
+            })
+    }, [setCourses, token]);
+    useEffect(() => {
+        fetch("/api/v1/lessons", {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setLessons(data);
+            })
+    }, [setLessons, token]);
+    useEffect(() => {
+        fetch("/api/v1/quizzes", {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setQuizzes(data);
+            })
+    }, [setQuizzes, token]);
+    useEffect(() => {
+        fetch("/api/v1/questions", {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setQuestions(data);
+            })
+    }, [setQuestions, token]);
+
+    useEffect(() => {
+        if (currentTable === 'Courses') {
+            setCurrentContent(courses);
+        }
+    }, [courses, currentTable, setCurrentContent]);
+
 
     useEffect(() => {
         if (role == null || role === "User") {
