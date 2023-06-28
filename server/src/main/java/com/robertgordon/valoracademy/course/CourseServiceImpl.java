613888f3f2b4db4954bad11d7cab359b56e741a2
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-28 07:49:18 -0700

    Adds implementation of updateIsUserFinished method to update the isUserFinished field

--- server/src/main/java/com/robertgordon/valoracademy/course/CourseServiceImpl.java
+++ server/src/main/java/com/robertgordon/valoracademy/course/CourseServiceImpl.java
@@ -7,26 +7,37 @@ import org.springframework.stereotype.Service;
 
 import com.robertgordon.valoracademy.exception.ResourceNotFoundException;
 
-/** The `@Service` annotation is used to indicate that a class is a business
- * service layer component. It is used to provide a clear separation between the 
- * business logic and the presentation layer. */
+/**
+ * The `@Service` annotation is used to indicate that a class is a business
+ * service layer component. It is used to provide a clear separation between the
+ * business logic and the presentation layer.
+ */
 @Service
 public class CourseServiceImpl implements CourseService {
 
-    /** `@Autowired` is injecting an instance of `CourseRepository` into the
-     *  `CourseServiceImpl` class, allowing it to access the methods defined in the
-     *  `CourseRepository` interface. */
+    /**
+     * `@Autowired` is injecting an instance of `CourseRepository` into the
+     * `CourseServiceImpl` class, allowing it to access the methods defined in the
+     * `CourseRepository` interface.
+     */
     @Autowired
     private CourseRepository courseRepository;
 
     /**
-     * This method saves a course object in the course repository and returns the saved course.
-     * @param course The parameter "course" is an object of the class "Course" that needs to be saved
-     * in the database. The method "saveCourse" takes this object as input and returns the saved
-     * object. The "courseRepository" is an instance of a repository interface that provides methods to
-     * interact with the database
-     * @return The method `saveCourse` is returning an instance of the `Course` class that has been
-     * saved to the database using the `save` method of the `courseRepository`.
+     * This method saves a course object in the course repository and returns the
+     * saved course.
+     * 
+     * @param course The parameter "course" is an object of the class "Course" that
+     *               needs to be saved
+     *               in the database. The method "saveCourse" takes this object as
+     *               input and returns the saved
+     *               object. The "courseRepository" is an instance of a repository
+     *               interface that provides methods to
+     *               interact with the database
+     * @return The method `saveCourse` is returning an instance of the `Course`
+     *         class that has been
+     *         saved to the database using the `save` method of the
+     *         `courseRepository`.
      */
     @Override
     public Course saveCourse(Course course) {
@@ -45,12 +56,17 @@ public class CourseServiceImpl implements CourseService {
     }
 
     /**
-     * This method retrieves a course by its ID from a repository and throws an exception if it
+     * This method retrieves a course by its ID from a repository and throws an
+     * exception if it
      * does not exist.
-     * @param courseId The ID of the course that we want to retrieve from the database.
-     * @return The method `getCourseById` returns a `Course` object with the specified `courseId`. If
-     * the course with the given `courseId` does not exist, it throws a `ResourceNotFoundException`
-     * with a message indicating that the course does not exist.
+     * 
+     * @param courseId The ID of the course that we want to retrieve from the
+     *                 database.
+     * @return The method `getCourseById` returns a `Course` object with the
+     *         specified `courseId`. If
+     *         the course with the given `courseId` does not exist, it throws a
+     *         `ResourceNotFoundException`
+     *         with a message indicating that the course does not exist.
      */
     @Override
     public Course getCourseById(Long courseId) {
@@ -58,13 +74,17 @@ public class CourseServiceImpl implements CourseService {
                 .orElseThrow(() -> new ResourceNotFoundException("Course does not exist with the ID: " + courseId));
     }
 
-   /**
-    * This method updates an existing course with new information and returns the updated course.
-    * @param id The ID of the course that needs to be updated.
-    * @param course The `course` parameter is an instance of the `Course` class that contains the
-    * updated information for the course that needs to be updated in the database.
-    * @return The updated course object is being returned.
-    */
+    /**
+     * This method updates an existing course with new information and returns the
+     * updated course.
+     * 
+     * @param id     The ID of the course that needs to be updated.
+     * @param course The `course` parameter is an instance of the `Course` class
+     *               that contains the
+     *               updated information for the course that needs to be updated in
+     *               the database.
+     * @return The updated course object is being returned.
+     */
     @Override
     public Course updateCourse(Long id, Course course) {
         Course existingCourse = courseRepository.findById(id)
@@ -80,7 +100,8 @@ public class CourseServiceImpl implements CourseService {
     }
 
     /**
-     * This method deletes a course with a given ID from a repository, throwing an exception if
+     * This method deletes a course with a given ID from a repository, throwing an
+     * exception if
      * the course does not exist.
      * 
      * @param id The ID of the course that needs to be deleted.
@@ -100,10 +121,29 @@ public class CourseServiceImpl implements CourseService {
      */
     @Override
     public void updatePublished(Long courseId) {
-        
-        Course existingCourse = courseRepository.findById(courseId).orElseThrow(() -> new ResourceNotFoundException("Course does not exist with ID: " + courseId));
+
+        Course existingCourse = courseRepository.findById(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException("Course does not exist with ID: " + courseId));
 
         existingCourse.setPublished(!existingCourse.isPublished());
+
+        courseRepository.save(existingCourse);
+    }
+
+    /**
+     * The method updates the "isUserFinished" property of a course to true.
+     * 
+     * @param courseId The courseId parameter is the unique identifier of the course
+     *                 that needs to be
+     *                 updated.
+     */
+    @Override
+    public void updateIsUserFinished(long courseId) {
+
+        Course existingCourse = courseRepository.findById(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException("Course does not exist with ID: " + courseId));
+
+        existingCourse.setUserFinished(true);
 
         courseRepository.save(existingCourse);
     }
