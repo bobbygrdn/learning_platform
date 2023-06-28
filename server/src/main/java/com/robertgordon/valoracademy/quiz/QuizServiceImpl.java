33d98e12ef3e650e8dc65460862498957fc236fe
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-28 07:52:54 -0700

    Adds implementation for updateIsUserFinished to update the isUserFinshed field

--- server/src/main/java/com/robertgordon/valoracademy/quiz/QuizServiceImpl.java
+++ server/src/main/java/com/robertgordon/valoracademy/quiz/QuizServiceImpl.java
@@ -7,24 +7,31 @@ import org.springframework.stereotype.Service;
 
 import com.robertgordon.valoracademy.exception.ResourceNotFoundException;
 
-/** The `@Service` annotation is used to indicate that a class is a business
- * service layer component. It is used to provide a clear separation between the 
- * business logic and the presentation layer. */
+/**
+ * The `@Service` annotation is used to indicate that a class is a business
+ * service layer component. It is used to provide a clear separation between the
+ * business logic and the presentation layer.
+ */
 @Service
 public class QuizServiceImpl implements QuizService {
 
-    /** `@Autowired` is injecting an instance of `QuizRepository` into the
-     *  `QuizServiceImpl` class, allowing it to access the methods defined in the
-     *  `QuizRepository` interface. */
+    /**
+     * `@Autowired` is injecting an instance of `QuizRepository` into the
+     * `QuizServiceImpl` class, allowing it to access the methods defined in the
+     * `QuizRepository` interface.
+     */
     @Autowired
     private QuizRepository quizRepository;
 
     /**
      * This method saves a quiz object to the quiz repository.
-     * @param quiz The parameter "quiz" is an object of the class Quiz that contains 
-     * information about a quiz. This method saves the quiz object to the database 
-     * using the quizRepository. The @Override annotation indicates that this method 
-     * overrides a method in a superclass or interface.
+     * 
+     * @param quiz The parameter "quiz" is an object of the class Quiz that contains
+     *             information about a quiz. This method saves the quiz object to
+     *             the database
+     *             using the quizRepository. The @Override annotation indicates that
+     *             this method
+     *             overrides a method in a superclass or interface.
      * @return The method `saveQuiz` returns an object of type `Quiz`.
      */
     @Override
@@ -44,12 +51,14 @@ public class QuizServiceImpl implements QuizService {
 
     /**
      * This method retrieves a quiz by its ID from a repository and throws an
-     *  exception if it does not exist.
-     * @param quizId quizId is a Long type parameter representing the unique 
-     * identifier of a quiz that needs to be retrieved from the database.
-     * @return The method is returning a Quiz object with the specified quizId. If 
-     * the quiz with the given quizId does not exist, it throws a 
-     * ResourceNotFoundException.
+     * exception if it does not exist.
+     * 
+     * @param quizId quizId is a Long type parameter representing the unique
+     *               identifier of a quiz that needs to be retrieved from the
+     *               database.
+     * @return The method is returning a Quiz object with the specified quizId. If
+     *         the quiz with the given quizId does not exist, it throws a
+     *         ResourceNotFoundException.
      */
     @Override
     public Quiz getQuizById(Long quizId) {
@@ -59,12 +68,14 @@ public class QuizServiceImpl implements QuizService {
 
     /**
      * This method updates an existing quiz with new title and description values.
-     * @param id The ID of the quiz that needs to be updated.
-     * @param quiz The "quiz" parameter is an instance of the Quiz class that 
-     * contains the updated information for an existing quiz. It is used to update 
-     * the title and description of the existing quiz.
-     * @return The method is returning an updated Quiz object after modifying its 
-     * title and description.
+     * 
+     * @param id   The ID of the quiz that needs to be updated.
+     * @param quiz The "quiz" parameter is an instance of the Quiz class that
+     *             contains the updated information for an existing quiz. It is used
+     *             to update
+     *             the title and description of the existing quiz.
+     * @return The method is returning an updated Quiz object after modifying its
+     *         title and description.
      */
     @Override
     public Quiz updateQuiz(Long id, Quiz quiz) {
@@ -78,7 +89,7 @@ public class QuizServiceImpl implements QuizService {
     }
 
     /**
-     * This method deletes a quiz with a given ID from a repository, throwing an 
+     * This method deletes a quiz with a given ID from a repository, throwing an
      * exception if the quiz does not exist.
      * 
      * @param id The ID of the quiz that needs to be deleted.
@@ -91,17 +102,37 @@ public class QuizServiceImpl implements QuizService {
         quizRepository.delete(quiz);
     }
 
-   /**
-    * This method updates the published status of a quiz by toggling its current value.
-    * 
-    * @param quizId The ID of the quiz that needs to be updated.
-    */
+    /**
+     * This method updates the published status of a quiz by toggling its current
+     * value.
+     * 
+     * @param quizId The ID of the quiz that needs to be updated.
+     */
     @Override
     public void updatePublished(long quizId) {
-        
-        Quiz existingQuiz = quizRepository.findById(quizId).orElseThrow(() -> new ResourceNotFoundException("Quiz does not exist with ID: " + quizId));
+
+        Quiz existingQuiz = quizRepository.findById(quizId)
+                .orElseThrow(() -> new ResourceNotFoundException("Quiz does not exist with ID: " + quizId));
 
         existingQuiz.setPublished(!existingQuiz.isPublished());
+
+        quizRepository.save(existingQuiz);
+    }
+
+    /**
+     * The method updates the "isUserFinished" field of a quiz with the given ID to
+     * true.
+     * 
+     * @param quizId The quizId parameter is the unique identifier of the quiz that
+     *               needs to be updated.
+     */
+    @Override
+    public void updateIsUserFinished(long quizId) {
+
+        Quiz existingQuiz = quizRepository.findById(quizId)
+                .orElseThrow(() -> new ResourceNotFoundException("Quiz does not exist with ID: " + quizId));
+
+        existingQuiz.setUserFinished(true);
 
         quizRepository.save(existingQuiz);
     }
--- server/src/main/java/com/robertgordon/valoracademy/quiz_question/Quiz_QuestionServiceImpl.java
+++ server/src/main/java/com/robertgordon/valoracademy/quiz_question/Quiz_QuestionServiceImpl.java
@@ -7,52 +7,67 @@ import org.springframework.stereotype.Service;
 
 import com.robertgordon.valoracademy.exception.ResourceNotFoundException;
 
-/** The `@Service` annotation is used to indicate that a class is a business
- * service layer component. It is used to provide a clear separation between the 
- * business logic and the presentation layer. */
+/**
+ * The `@Service` annotation is used to indicate that a class is a business
+ * service layer component. It is used to provide a clear separation between the
+ * business logic and the presentation layer.
+ */
 @Service
 public class Quiz_QuestionServiceImpl implements Quiz_QuestionService {
 
-    /** `@Autowired` is injecting an instance of `Quiz_QuestionRepository` into the
-     *  `Quiz_QuestionServiceImpl` class, allowing it to access the methods defined in the
-     *  `Quiz_QuestionRepository` interface. */
+    /**
+     * `@Autowired` is injecting an instance of `Quiz_QuestionRepository` into the
+     * `Quiz_QuestionServiceImpl` class, allowing it to access the methods defined
+     * in the
+     * `Quiz_QuestionRepository` interface.
+     */
     @Autowired
     private Quiz_QuestionRepository quiz_QuestionRepository;
 
     /**
-     * This method saves a Quiz_Question object to the database using the Quiz_QuestionRepository.
-     * @param quiz_Question quiz_Question is an object of type Quiz_Question that 
-     * contains the data for a quiz question. This method saves the quiz question to
-     * the database using the quiz_QuestionRepository. The @Override annotation
-     * indicates that this method is overriding a method in a superclass or interface.
-     * @return The method `saveQuiz_Question` is returning an object of type 
-     * `Quiz_Question`. This object is obtained by calling the `save` method on the 
-     * `quiz_QuestionRepository` object with the `quiz_Question` parameter passed to 
-     * the method.
+     * This method saves a Quiz_Question object to the database using the
+     * Quiz_QuestionRepository.
+     * 
+     * @param quiz_Question quiz_Question is an object of type Quiz_Question that
+     *                      contains the data for a quiz question. This method saves
+     *                      the quiz question to
+     *                      the database using the quiz_QuestionRepository.
+     *                      The @Override annotation
+     *                      indicates that this method is overriding a method in a
+     *                      superclass or interface.
+     * @return The method `saveQuiz_Question` is returning an object of type
+     *         `Quiz_Question`. This object is obtained by calling the `save` method
+     *         on the
+     *         `quiz_QuestionRepository` object with the `quiz_Question` parameter
+     *         passed to
+     *         the method.
      */
     @Override
     public Quiz_Question saveQuiz_Question(Quiz_Question quiz_Question) {
         return quiz_QuestionRepository.save(quiz_Question);
     }
 
-   /**
-    * This method returns a list of all quiz questions from the quiz_QuestionRepository.
-    * 
-    * @return A list of Quiz_Question objects is being returned.
-    */
+    /**
+     * This method returns a list of all quiz questions from the
+     * quiz_QuestionRepository.
+     * 
+     * @return A list of Quiz_Question objects is being returned.
+     */
     @Override
     public List<Quiz_Question> getAllQuiz_Questions() {
         return quiz_QuestionRepository.findAll();
     }
 
     /**
-     * This method retrieves a Quiz_Question object by its ID from a repository and 
+     * This method retrieves a Quiz_Question object by its ID from a repository and
      * throws a ResourceNotFoundException if it does not exist.
-     * @param id The parameter "id" is a Long data type representing the unique 
-     * identifier of a Quiz_Question object that needs to be retrieved from the 
-     * database.
-     * @return The method is returning a Quiz_Question object with the specified ID. 
-     * If the object does not exist, it throws a ResourceNotFoundException.
+     * 
+     * @param id The parameter "id" is a Long data type representing the unique
+     *           identifier of a Quiz_Question object that needs to be retrieved
+     *           from the
+     *           database.
+     * @return The method is returning a Quiz_Question object with the specified ID.
+     *         If the object does not exist, it throws a ResourceNotFoundException.
      */
     @Override
     public Quiz_Question getQuiz_QuestionById(Long id) {
@@ -62,11 +77,14 @@ public class Quiz_QuestionServiceImpl implements Quiz_QuestionService {
 
     /**
      * This method updates an existing quiz question with new title and content.
-     * @param id The ID of the Quiz_Question that needs to be updated.
+     * 
+     * @param id            The ID of the Quiz_Question that needs to be updated.
      * @param quiz_Question The parameter `quiz_Question` is an object of type
-     * `Quiz_Question` which contains the updated information for a quiz question.
-     * @return The method is returning an updated Quiz_Question object after updating
-     * its title and content.
+     *                      `Quiz_Question` which contains the updated information
+     *                      for a quiz question.
+     * @return The method is returning an updated Quiz_Question object after
+     *         updating
+     *         its title and content.
      */
     @Override
     public Quiz_Question updateQuiz_Question(Long id, Quiz_Question quiz_Question) {
@@ -81,8 +99,10 @@ public class Quiz_QuestionServiceImpl implements Quiz_QuestionService {
 
     /**
      * This method deletes a quiz question by its ID.
-     * @param id The parameter "id" is a Long data type representing the unique 
-     * identifier of a Quiz_Question object that needs to be deleted from the database.
+     * 
+     * @param id The parameter "id" is a Long data type representing the unique
+     *           identifier of a Quiz_Question object that needs to be deleted from
+     *           the database.
      */
     @Override
     public void deleteQuiz_Question(Long id) {
@@ -93,16 +113,38 @@ public class Quiz_QuestionServiceImpl implements Quiz_QuestionService {
     }
 
     /**
-     * This method updates the published status of a quiz question in a Java application.
+     * This method updates the published status of a quiz question in a Java
+     * application.
      * 
-     * @param questionId The ID of the Quiz_Question object that needs to be updated.
+     * @param questionId The ID of the Quiz_Question object that needs to be
+     *                   updated.
      */
     @Override
     public void updatePublished(long questionId) {
-        
-        Quiz_Question existingQuestion = quiz_QuestionRepository.findById(questionId).orElseThrow(() -> new ResourceNotFoundException("Question does not exist with ID: " + questionId));
+
+        Quiz_Question existingQuestion = quiz_QuestionRepository.findById(questionId)
+                .orElseThrow(() -> new ResourceNotFoundException("Question does not exist with ID: " + questionId));
 
         existingQuestion.setPublished(!existingQuestion.isPublished());
+
+        quiz_QuestionRepository.save(existingQuestion);
+    }
+
+    /**
+     * The method updates the "isUserFinished" field of a Quiz_Question object to
+     * true.
+     * 
+     * @param questionId The questionId parameter is the unique identifier of the
+     *                   question that needs
+     *                   to be updated.
+     */
+    @Override
+    public void updateIsUserFinished(long questionId) {
+
+        Quiz_Question existingQuestion = quiz_QuestionRepository.findById(questionId)
+                .orElseThrow(() -> new ResourceNotFoundException("Question does not exist with ID: " + questionId));
+
+        existingQuestion.setUserFinished(true);
 
         quiz_QuestionRepository.save(existingQuestion);
     }
