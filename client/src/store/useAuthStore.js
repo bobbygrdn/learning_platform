64c51d60b8caf1ea3446af5789c2e5f979fa5ab8
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-30 15:32:51 -0700

    Adds the new state for the progression system

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -4,12 +4,24 @@ const useAuthStore = createStore((set) => {
 
     const token = localStorage.getItem('token');
     const role = localStorage.getItem('role');
+    const experience = localStorage.getItem('experience');
+    const learningStreak = localStorage.getItem('learningStreak');
+    const title = localStorage.getItem('title');
+    const mastery = localStorage.getItem('mastery');
 
     return {
         role: role || null,
         setRole: (role) => set(() => ({ role })),
         token: token || null,
         setToken: (token) => set(() => ({ token })),
+        experience: experience || null,
+        setExperience: (experience) => set(() => ({ experience })),
+        learningStreak: learningStreak || null,
+        setLearningStreak: (learningStreak) => set(() => ({ learningStreak })),
+        title: title || null,
+        setTitle: (title) => set(() => ({ title })),
+        mastery: mastery || null,
+        setMastery: (mastery) => set(() => ({ mastery })),
         allowRegistration: false,
         setAllowRegistration: (allowRegistration) => set(() => ({ allowRegistration: allowRegistration })),
     }
