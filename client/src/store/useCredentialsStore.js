761fe22cdc2b99bbe787ab52314d20fa23d7ea9b
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-30 15:33:11 -0700

    Adds the default value for the username to be pulled from localstorage if possible

--- client/src/store/useCredentialsStore.js
+++ client/src/store/useCredentialsStore.js
@@ -1,16 +1,21 @@
 import { createStore } from 'zustand';
 
-const useCredentialStore = createStore((set) => ({
-    userName: '',
-    password: '',
-    userId: 0,
-    keepLoggedIn: false,
-    disabled: true,
-    setUserName: (name) => set({ userName: name }),
-    setPassword: (pass) => set({ password: pass }),
-    setUserId: (id) => set({ userId: id }),
-    setKeepLoggedIn: (keep) => set({ keepLoggedIn: keep }),
-    setDisabled: (disable) => set({ disabled: disable }),
-}));
+const useCredentialStore = createStore((set) => {
+
+    const userName = localStorage.getItem('username');
+
+    return {
+        userName: userName || '',
+        password: '',
+        userId: 0,
+        keepLoggedIn: false,
+        disabled: true,
+        setUserName: (name) => set({ userName: name }),
+        setPassword: (pass) => set({ password: pass }),
+        setUserId: (id) => set({ userId: id }),
+        setKeepLoggedIn: (keep) => set({ keepLoggedIn: keep }),
+        setDisabled: (disable) => set({ disabled: disable }),
+    }
+});
 
 export default useCredentialStore;
