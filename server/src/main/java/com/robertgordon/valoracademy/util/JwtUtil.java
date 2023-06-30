fa43bc6de44bf57a57ce25b160c3332ddb0b83be
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-30 15:35:31 -0700

    Adds the username, experience, learningstreak, title, and mastery fields to be returned to the client upon successful login inside the jwtTokenMap

--- server/src/main/java/com/robertgordon/valoracademy/util/JwtUtil.java
+++ server/src/main/java/com/robertgordon/valoracademy/util/JwtUtil.java
@@ -12,22 +12,32 @@ import com.robertgordon.valoracademy.user.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-/** `@Component` is used to mark the `JwtUtil` class as a Spring component so that it can be injected into other 
-* Spring-managed classes using dependency injection. */
+/**
+ * `@Component` is used to mark the `JwtUtil` class as a Spring component so
+ * that it can be injected into other
+ * Spring-managed classes using dependency injection.
+ */
 @Component
 public class JwtUtil {
 
-   /** `@Value("${jwt.secret}")` is a Spring annotation used to inject the value of the `jwt.secret`
-    * property from the application properties file into the `secret` variable. This allows the
-    * `secret` value to be easily changed without modifying the code. */
+    /**
+     * `@Value("${jwt.secret}")` is a Spring annotation used to inject the value of
+     * the `jwt.secret`
+     * property from the application properties file into the `secret` variable.
+     * This allows the
+     * `secret` value to be easily changed without modifying the code.
+     */
     @Value("${jwt.secret}")
     private String secret;
 
+    
     /**
-     * This method generates a JWT token for a given user and returns a map containing the token, user
-     * ID, and user role.
-     * @param existingUser an object of the User class that represents an existing user in the system.
-     * @return A Map object containing a JWT token, user ID, and user role.
+     * The method generates a JWT token and returns a map containing the token and user information.
+     * 
+     * @param existingUser The existingUser parameter is an object of the User class. It represents the
+     * user for whom the token is being generated. The User class likely has properties such as id,
+     * username, role, title, experience, learningStreak, and mastery.
+     * @return The method is returning a Map<String, String> object.
      */
     public Map<String, String> generateToken(User existingUser) {
 
@@ -38,6 +48,12 @@ public class JwtUtil {
         jwtTokenMap.put("token", jwtToken);
         jwtTokenMap.put("id", String.valueOf(existingUser.getId()));
         jwtTokenMap.put("role", existingUser.getRole());
+        jwtTokenMap.put("username", existingUser.getUsername());
+        jwtTokenMap.put("title", existingUser.getTitle());
+        jwtTokenMap.put("experience", String.valueOf(existingUser.getExperience()));
+        jwtTokenMap.put("learningStreak", String.valueOf(existingUser.getLearningStreak()));
+        jwtTokenMap.put("mastery", existingUser.getMastery());
+
         return jwtTokenMap;
     }
 
