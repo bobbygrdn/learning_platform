b23225be6de2fa68b0d90d46da01d1abc360d1b2
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-07-04 16:05:36 -0700

    Adds the logic for the resetPassword and enrollInCourse methods. Also fixes the comments for better readability

--- server/src/main/java/com/robertgordon/valoracademy/user/UserServiceImpl.java
+++ server/src/main/java/com/robertgordon/valoracademy/user/UserServiceImpl.java
@@ -5,31 +5,45 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.robertgordon.valoracademy.course.Course;
+import com.robertgordon.valoracademy.course.CourseRepository;
 import com.robertgordon.valoracademy.exception.ResourceNotFoundException;
 import com.robertgordon.valoracademy.util.PasswordEncoder;
 
-/** The `@Service` annotation is used to indicate that a class is a business
- * service layer component. It is used to provide a clear separation between the 
- * business logic and the presentation layer. */
+/**
+ * The `@Service` annotation is used to indicate that a class is a business
+ * service layer component. It is used to provide a clear separation between the
+ * business logic and the presentation layer.
+ */
 @Service
 public class UserServiceImpl implements UserService {
 
-    /** `@Autowired` is injecting an instance of `UserRepository` into the
-     *  `UserServiceImpl` class, allowing it to access the methods defined in the
-     *  `UserRepository` interface. */
+    /**
+     * `@Autowired` is injecting an instance of `UserRepository` into the
+     * `UserServiceImpl` class, allowing it to access the methods defined in the
+     * `UserRepository` interface.
+     */
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private CourseRepository courseRepository;
+
     /**
-     * This method saves a user by encoding their password and then saving them to the
-     *  user repository.
-     *  @param user The "user" parameter is an instance of the User class, which 
-     * contains information about a user such as their name, email, and password. The
-     *  method is taking this user object as input and saving it to a database using
-     *  the userRepository. Before saving, the method is encoding the user's password 
-     * using
-     * @return The method is returning a User object that has been saved in the 
-     * userRepository after encoding the password.
+     * This method saves a user by encoding their password and then saving them to
+     * the
+     * user repository.
+     * 
+     * @param user The "user" parameter is an instance of the User class, which
+     *             contains information about a user such as their name, email, and
+     *             password. The
+     *             method is taking this user object as input and saving it to a
+     *             database using
+     *             the userRepository. Before saving, the method is encoding the
+     *             user's password
+     *             using
+     * @return The method is returning a User object that has been saved in the
+     *         userRepository after encoding the password.
      */
     @Override
     public User saveUser(User user) {
@@ -41,8 +55,8 @@ public class UserServiceImpl implements UserService {
     /**
      * This method returns a list of all users from the user repository.
      * 
-     * @return A list of User objects is being returned. The list contains all the 
-     * users that are stored in the userRepository.
+     * @return A list of User objects is being returned. The list contains all the
+     *         users that are stored in the userRepository.
      */
     @Override
     public List<User> getAllUsers() {
@@ -50,15 +64,19 @@ public class UserServiceImpl implements UserService {
     }
 
     /**
-     * This method retrieves a user by their ID from a user repository and throws a 
+     * This method retrieves a user by their ID from a user repository and throws a
      * resource not found exception if the user does not exist.
+     * 
      * @param userId userId is a Long type variable that represents the unique
-     *  identifier of a user. It is used as a parameter to retrieve a specific user 
-     * from the database.
-     * @return The method is returning a User object with the specified userId. If the
-     * user with the given userId is not found in the userRepository, then a 
-     * ResourceNotFoundException is thrown with a message indicating that the user 
-     * does not exist with the given ID.
+     *               identifier of a user. It is used as a parameter to retrieve a
+     *               specific user
+     *               from the database.
+     * @return The method is returning a User object with the specified userId. If
+     *         the
+     *         user with the given userId is not found in the userRepository, then a
+     *         ResourceNotFoundException is thrown with a message indicating that
+     *         the user
+     *         does not exist with the given ID.
      */
     @Override
     public User getUserById(Long userId) {
@@ -67,14 +85,17 @@ public class UserServiceImpl implements UserService {
     }
 
     /**
-     * This method updates an existing user's username, email, and password in the 
+     * This method updates an existing user's username, email, and password in the
      * database.
-     * @param id The ID of the user that needs to be updated in the database.
-     * @param user The "user" parameter is an instance of the User class that contains
-     *  the updated information for an existing user. It includes the new username, 
-     * email, and password for the user.
-     * @return The method is returning an updated User object after updating the 
-     * username, email, and password of an existing user in the database.
+     * 
+     * @param id   The ID of the user that needs to be updated in the database.
+     * @param user The "user" parameter is an instance of the User class that
+     *             contains
+     *             the updated information for an existing user. It includes the new
+     *             username,
+     *             email, and password for the user.
+     * @return The method is returning an updated User object after updating the
+     *         username, email, and password of an existing user in the database.
      */
     @Override
     public User updateUser(Long id, User user) {
@@ -89,16 +110,21 @@ public class UserServiceImpl implements UserService {
         return userRepository.save(existingUser);
     }
 
-   /**
-    * This method updates the role of an existing user in the database.
-    * @param id The ID of the user that needs to be updated.
-    * @param user The "user" parameter is an instance of the User class, which contains 
-    * information about a user such as their name, email, password, and role. In this 
-    * method, the "user" parameter is used to update the role of an existing user in 
-    * the database.
-    * @return The method is returning an updated User object with the new role that was
-    * set.
-    */
+    /**
+     * This method updates the role of an existing user in the database.
+     * 
+     * @param id   The ID of the user that needs to be updated.
+     * @param user The "user" parameter is an instance of the User class, which
+     *             contains
+     *             information about a user such as their name, email, password, and
+     *             role. In this
+     *             method, the "user" parameter is used to update the role of an
+     *             existing user in
+     *             the database.
+     * @return The method is returning an updated User object with the new role that
+     *         was
+     *         set.
+     */
     @Override
     public User updateUserRole(Long id, User user) {
         User existingUser = userRepository.findById(id)
@@ -112,8 +138,9 @@ public class UserServiceImpl implements UserService {
     /**
      * This method deletes a user with a given ID from a user repository, throwing a
      * resource not found exception if the user does not exist.
-     * @param id The id parameter is a Long value representing the unique identifier 
-     * of the user that needs to be deleted from the database.
+     * 
+     * @param id The id parameter is a Long value representing the unique identifier
+     *           of the user that needs to be deleted from the database.
      */
     @Override
     public void deleteUser(Long id) {
@@ -121,6 +148,50 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("User does not exist with the ID: " + id));
 
         userRepository.delete(user);
+    }
+
+    /**
+     * The method resets the password for a user with the given ID and returns the
+     * updated user object.
+     * 
+     * @param userId   The userId parameter is the unique identifier of the user
+     *                 whose password needs to be reset.
+     * @param password The "password" parameter is a String that represents the
+     *                 new password that will be set for the user.
+     * @return The method is returning a User object.
+     */
+    @Override
+    public User resetPassword(long userId, String password) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with the ID: " + userId));
+
+        user.setPassword(PasswordEncoder.encodePassword(password));
+
+        return userRepository.save(user);
+    }
+
+    /**
+     * This method enrolls a user in a course by adding the course to the user's
+     * list of courses and saving the updated user object.
+     * 
+     * @param userId   The ID of the user who wants to enroll in the course.
+     * @param courseId The courseId parameter is the unique identifier of the course
+     *                 that the user wants to enroll in.
+     * @return The method is returning a User object.
+     */
+    @Override
+    public User enrollInCourse(long userId, long courseId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with the ID: " + userId));
+
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException("Course does not exist with the ID: " + courseId));
+
+        user.addCourse(course);
+
+        return userRepository.save(user);
     }
 
 }
