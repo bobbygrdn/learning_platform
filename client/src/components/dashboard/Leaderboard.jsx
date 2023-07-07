20277836624c71b6117d972226c84687434670b9
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-07-06 18:24:31 -0700

    Finishes the leaderboard component to display all the top scores for the platform

--- client/src/components/dashboard/Leaderboard.jsx
+++ client/src/components/dashboard/Leaderboard.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import LeaderboardNavBar from './LeaderboardNavBar';
+import '../../styles/dashboard/Leaderboard.css'
+import { useStore } from 'zustand';
+import useTableStore from '../../store/useTableStore';
 
 export default function Leaderboard() {
+    const [leaderboardStats, setLeaderboardStats] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const { leaderboard } = useStore(useTableStore);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/dummyData/leaderboardStats.json');
+            const data = await response.json();
+            setLeaderboardStats(data);
+            setLoading(false);
+        };
+        fetchData();
+    }, [])
+
+    let stats = leaderboardStats[leaderboard]
+
     return (
-        <div>
-            <h1>Leaderboard</h1>
+        <div className='leaderboardContainer'>
+            <LeaderboardNavBar />
+            <section className='leaderboardStats'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Username</th>
+                            <th>Experience</th>
+                            <th>Badges</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* eslint-disable-next-line array-callback-return */}
+                        {stats && stats.map((element, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{element.username}</td>
+                                <td>{element.experience}</td>
+                                <td>{element.badges}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </section>
         </div>
     )
 }
+
