b57af72a809cc4731ca96df5f9d06da0c223eec9
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-07-08 16:02:00 -0700

    Removes the logic for using toast and brings the fetch logic to the parent CharacterCreator component to keep this component simple

--- client/src/components/dashboard/characterCreator/FinalizeCharacter.jsx
+++ client/src/components/dashboard/characterCreator/FinalizeCharacter.jsx
@@ -1,67 +1,16 @@
-import React, { useEffect } from 'react';
-import { toast } from 'react-toastify';
+import React from 'react';
 import { useStore } from 'zustand';
-import useAuthStore from '../../../store/useAuthStore';
-import useCredentialStore from '../../../store/useCredentialsStore';
 import useWarriorStore from '../../../store/useWarriorStore';
 
 export default function FinalizeCharacter() {
 
-    const { token } = useStore(useAuthStore);
-    const { userId } = useStore(useCredentialStore);
     const { genderSelection, characterSelection } = useStore(useWarriorStore);
-
-    const handleCloseToast = () => {
-        toast.dismiss()
-    }
-
-    const finalizeCharacterSelection = () => {
-        toast.info(
-            <div>
-                <h3>Is this the warrior you want to choose?</h3>
-                <div className='buttons'>
-                    <button className='yes' onClick={handleWarriorSelection}>Submit</button>
-                    <button className='no' onClick={handleCloseToast}>Cancel</button>
-                </div>
-            </div>,
-            {
-                autoClose: false,
-                closeOnClick: false,
-            }
-        )
-    }
-
-    const handleWarriorSelection = () => {
-        handleCloseToast();
-        fetch(`/api/v1/users/${userId}/warriorSelect`,
-            {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                    'Authorization': `Bearer ${token}`
-                },
-                body: JSON.stringify({
-                    title: genderSelection + characterSelection
-                }),
-            })
-            .then((response) => response.json())
-            .then(data => {
-                toast.success("You have successfully chosen your warrior!");
-            })
-            .catch(error => {
-                toast.error("Your warrior could not be chosen!");
-            })
-    }
-
-    useEffect(() => {
-        finalizeCharacterSelection();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
 
     return (
         <div className='character-container'>
-            <div id='male' className='characterCard selected'>
+            <div id='male' className='characterCard selected finalizeCharacter'>
                 <img className='warrior' src={`/resources/${genderSelection}${characterSelection}.jpg`} alt='warrior'></img>
+                <h3 className='finalizeText'>Is this the warrior you would like to select?</h3>
             </div>
         </div>
     )
