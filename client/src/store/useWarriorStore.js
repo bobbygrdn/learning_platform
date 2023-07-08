9024bfd6b9a88e5ce4caf16b655a45ed9063494c
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-07-08 15:57:19 -0700

    Adds the warrior lore and chosen warrior state to keep track of the warrior the user chooses and the lore to show for the user to view

--- client/src/store/useWarriorStore.js
+++ client/src/store/useWarriorStore.js
@@ -3,8 +3,12 @@ import { createStore } from "zustand";
 const useWarriorStore = createStore((set) => ({
     characterSelection: null,
     genderSelection: null,
+    warriorLore: null,
+    chosenWarrior: null,
     setCharacterSelection: (characterSelection) => set({ characterSelection }),
     setGenderSelection: (genderSelection) => set({ genderSelection }),
+    setWarriorLore: (warriorLore) => set({ warriorLore }),
+    setChosenWarrior: (chosenWarrior) => set({ chosenWarrior })
 }));
 
 export default useWarriorStore;
